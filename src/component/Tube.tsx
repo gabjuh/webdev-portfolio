bd40bee5e863dc56a26366d1b34d2e9c273ed475
gabjuh <jjjjgabesz@gmail.com>
2023-02-26 06:22:50 +0100

    snake -> camel; import brandWidth and direction

--- src/component/Tube.tsx
+++ src/component/Tube.tsx
@@ -2,26 +2,40 @@ import React, { useState } from 'react';
 
 interface Bend {
   height: number,
+  branchWidth: number,
   color: string,
   strokeWidth: number;
+  direction: 'right' | 'left',
 }
 
 const Tube: React.FC<Bend> = ({
   height,
+  branchWidth,
   color,
   strokeWidth,
+  direction
 }) => {
+
+  const margin = branchWidth - strokeWidth;
 
   return (
     <>
-      <svg height={height} width={strokeWidth} className="">
+      <svg
+        height={height}
+        width={strokeWidth}
+        // className={direction === 'right' ? `ml-[${margin}px]` : `mr-[128px]`}
+        style={{
+          marginLeft: direction === 'right' ? `${margin}px` : 0,
+          marginRight: direction === 'left' ? `${margin}px` : 0
+        }}
+      >
         <path
           d={`
             M ${strokeWidth / 2} ${height} 
             l 0 -${height}
           `}
           stroke={color}
-          stroke-width={strokeWidth} />
+          strokeWidth={strokeWidth} />
       </svg>
     </>
   );
