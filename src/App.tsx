a4050315ae08930ebad80d5e26197e13f830ceeb
gabjuh <jjjjgabesz@gmail.com>
2023-02-27 11:56:00 +0100

    add colors and textColor

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import CvTree from './component/CvTree';
 
 function App() {
 
-  const bgColor = '#333';
+  const colors = ['#eee', '#222'];
+
+  const bgColor = colors[0];
+  const textColor = colors[1];
   const body = document.querySelector('body') as HTMLElement;
 
   body.style.backgroundColor = bgColor;
+  body.style.color = textColor;
 
   return (
     <div className="App">
       <CvTree
         bgColor={bgColor}
+        textColor={textColor}
       />
     </div>
   );
