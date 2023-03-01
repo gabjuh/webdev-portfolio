21ac3bb0a658f4536c62a5e71a5aaf92b33ab55e
gabjuh <jjjjgabesz@gmail.com>
2023-03-01 11:05:58 +0100

    cleanup, add length logic in render loop (temporary)

--- src/component/CvTree.tsx
+++ src/component/CvTree.tsx
@@ -2,12 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Branch from './Branch';
 import Tube from './Tube';
 import Point from './Point';
-import Item from './Item';
 import { Year, Text } from './Text';
-import * as data from '../cv.json';
-
-// type DataKeysGeneral = keyof typeof data.general;
-// const dataKeysGeneral: DataKeysGeneral[] = Object.keys(data) as DataKeysGeneral[];
+import * as data from '../cv_gj.json';
 
 interface ICvTree {
   bgColor: string;
@@ -29,17 +25,13 @@ const CvTree: React.FC<ICvTree> = ({
   const level: number = 2 * size; // level is actually the bright of the level of the branches
   const [jumpToLevel, setJumpToLevel] = useState<number>(0); // the number of how many braches will be jumped through
   const [strokeWidth, setStrokeWidth] = useState<number>(5); // simply the width of the Bend
-  const [direction, setDirection] = useState<'right' | 'left'>('right'); // direction of the bend
-  const height: number = level; // height is 2 times the size (hight), always the same.
-
-  const [color, setColor] = useState<string>('lightgreen'); // color of the Bend
+  // const height: number = level; // height is 2 times the size (hight), always the same.
 
   const width: number = (level + strokeWidth) + jumpToLevel * size; // width is 2 times the level plus the strokeWidth, and if jumpToLevel !== 0, it will be multiplicated by the size
-  const startPos: number = direction === 'right' ? strokeWidth : width; // set the starting position, default it is 0 + strokeWidht, or if direction is left, it is the width
 
-  const step = 40;
+  const step = 60;
 
-  // Stock
+  // Timeline
   const [stockHeight, setStockHeight] = useState<number>(step * data.items.length);
 
   // Svg
@@ -52,31 +44,27 @@ const CvTree: React.FC<ICvTree> = ({
     setStockHeight(step * data.items.length);
   }, []);
 
-
   // Sort items
   const [sortedItems, setSortedItems] = useState(data.items.sort((a, b) => a.content.year - b.content.year));
 
-  useEffect(() => {
-    // console.log('sorted items changed');
-    // console.log(sortedItems);
-  }, [sortedItems]);
-
+  // console.log('sorted', sortedItems);
+  // console.log('original', data.items);
 
   // Data modification loop:
   // - add end year if not there
   // - remove duplicates
-  data.items.map((_, i, a) => {
+  data.items.forEach((_, i, a) => {
     const item = data.items[a.length - 1 - i];
     const content = item.content;
 
     // const startIndex = sortedItems.findIndex(obj => obj.content.year === content.year);
     const endIndex = sortedItems.findIndex(obj => obj.content.year === content.end);
 
-    // I'm not sure, but it might comes in the other loop
-    if (content.end === undefined || content.year > new Date().getFullYear()) {
+    // If end year is not defined or ..
+    if (content.end === undefined) {
       // console.log('still active');
     } else if (endIndex === -1) {
-      // console.log('should be closed');
+      // console.log('should be closed', content.end);
 
       // If the year of the finishing of a project is not a starting point of another (so it would not be shown), it will be added without any additional text.
       data.items.push({
@@ -98,7 +86,6 @@ const CvTree: React.FC<ICvTree> = ({
     // There will be at least one match (itself) so if there is more, set showYear fo false
     if (duplicateIndexes.length > 1) {
       duplicateIndexes.forEach((nr, i) => {
-        console.log(i);
         if (i) { data.items[nr].content.showYear = false; }
       });
     }
@@ -127,47 +114,46 @@ const CvTree: React.FC<ICvTree> = ({
 
             {/* Loop of items */}
             {data.items.map((_, i, a) => {
-              const item = data.items[a.length - 1 - i];
+              const index = a.length - 1 - i;
+              // const index = i;
+              const item = data.items[index];
               const content = item.content;
               const layout = item.layout;
               const side = layout?.side;
               const yPos = step * (i + 1) + 6;
 
+              // Find the year object index, where the end year is
+              const endYearIndex = sortedItems.findIndex(obj => content.end && content.end === obj.content.year);
+
+              // Get the differenz - not in years but in index-numbers!
+              // const startEndDiff = index !== endYearIndex && endYearIndex > -1 ? index - endYearIndex : null;
+              const startEndDiff = endYearIndex && endYearIndex > -1 ? index - endYearIndex : 1;
+
+              console.log({ year: content.year, end: content.end, name: content.name, index, endYearIndex, startEndDiff });
+
               return (
                 <React.Fragment key={i}>
 
-                  {/* SIDEBRANCH */}
                   {/* Branch */}
                   {layout &&
                     <Branch
-                      id={`branch_${i}`}
-                      height={height}
-                      width={width}
+                      height={startEndDiff}
+                      step={step}
                       size={size}
                       side={side}
                       color={layout ? layout.color : 'red'}
                       strokeWidth={strokeWidth}
-                      level={level}
                       pos={[200, step * (i + 1) + step / 2.1]}
-                      startPos={stockStartPos + strokeWidth / 2}
-                      direction={direction}
-                      bgColor={bgColor}
-                      pointSize={pointSize}
-                      pointStrokeWidth={pointStrokeWidth}
                       jumpToLevel={jumpToLevel}
-                    // sortedItems={sortedItems}
-                    // setSortedItems={setSortedItems}
                     />
                   }
 
-                  {/* TEXTS */}
                   {/* Year */}
                   <Year
                     content={content}
                     textColor={textColor}
                     y={yPos}
                   />
-
 
                   {/* Name */}
                   <Text
@@ -180,14 +166,12 @@ const CvTree: React.FC<ICvTree> = ({
               );
             })}
 
-            {/* STACKLINE */}
             {/* Stack */}
             <Tube
               height={stockHeight}
               branchWidth={width}
               color={'orange'}
               strokeWidth={strokeWidth}
-              direction={direction}
               startPos={stockStartPos}
               isStock={true}
             />
