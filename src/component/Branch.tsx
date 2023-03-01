2a7c65208b7bffe476cb0123ebb301d0c6f08446
gabjuh <jjjjgabesz@gmail.com>
2023-03-01 18:40:03 +0100

    import Point, draw end if not finished

--- src/component/Branch.tsx
+++ src/component/Branch.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Point from './Point';
 
 interface IBranch {
   height: number | null,
@@ -10,6 +11,7 @@ interface IBranch {
   strokeWidth: number,
   pos: number[],
   jumpToLevel: number;
+  heightIfNotEndedYet: number | null;
 }
 
 const Branch: React.FC<IBranch> = ({
@@ -21,6 +23,7 @@ const Branch: React.FC<IBranch> = ({
   strokeWidth,
   pos,
   jumpToLevel,
+  heightIfNotEndedYet,
 }) => {
 
   // The first line of code turns to the branch 90deg to the right, the third one 90deg to the left. The code in the middle makes it longer between, if jumpToLevel > 0
@@ -37,19 +40,39 @@ const Branch: React.FC<IBranch> = ({
    q -${size} 0 -${size} -${size}
  `;
 
+  //  If branch has a finishing point, merge it to the timeline
+  const ifEnds: string = `
+    l 0 -${height ? height * -1 * step - step : step}
+    ${side === 'right' ? bendLeft : bendRight}
+  `;
+
+  // If branch has not a finishing point, so it is still active, it goes up to the top based on the remaining place to the top
+  const lineIfNotEndedYet: string = `
+    l 0 -${heightIfNotEndedYet ? heightIfNotEndedYet * step + size : step}
+ `;
+
   return (
     <>
       <path
         d={`
           M ${pos[0]} ${pos[1]}
           ${side === 'right' ? bendRight : bendLeft}
-          l 0 -${height ? height * -1 * step : step}
-          ${side === 'right' ? bendLeft : bendRight}
+          ${heightIfNotEndedYet ? lineIfNotEndedYet : ifEnds}
         `}
         stroke={color ? color : 'lightgray'}
         strokeWidth={strokeWidth}
         fill="none"
       />
+      {heightIfNotEndedYet && (
+        <Point
+          color={color}
+          isMajor={true}
+          bgColor={'#222'}
+          pos={[side === 'right' ? pos[0] + size * 2 : pos[0] - size * 2, 0]}
+          size={6}
+          strokeWidth={1.5}
+        />
+      )}
     </>
   );
 };
