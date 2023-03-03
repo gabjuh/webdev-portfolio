520976deff42fdd0c864a960c9a059fa6aa59304
gabjuh <jjjjgabesz@gmail.com>
2023-03-03 08:19:36 +0100

    cleanup, add vertical position

--- src/component/Tube.tsx
+++ src/component/Tube.tsx
@@ -1,28 +1,32 @@
 import React, { useState } from 'react';
 
 interface ITune {
-  height: number | string,
-  color: string,
+  height: number | string;
+  verticalPosition: number;
+  color: string;
   strokeWidth: number;
-  startPos?: number,
-  isStock?: Boolean,
+  startPos?: number;
+  isStock?: Boolean;
 }
 
 const Tube: React.FC<ITune> = ({
   height,
+  verticalPosition,
   color,
   strokeWidth,
   startPos,
   isStock,
 }) => {
 
-  const [vertPos, setVertPos] = useState<number>(startPos ? startPos : 0);
+  // const [vertPos, setVertPos] = useState<number>(startPos ? startPos : 0);
+
+  // M ${ !isStock ? vertPos + strokeWidth / 2 : vertPos; } ${ height; }
 
   return (
     <>
       <path
         d={`
-            M ${!isStock ? vertPos + strokeWidth / 2 : vertPos} ${height} 
+            M ${verticalPosition} ${height} 
             l 0 -${height}
           `}
         stroke={color}
