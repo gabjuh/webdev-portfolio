c75163ba7a8d37efafe012e54c2f34939678764f
gabjuh <jjjjgabesz@gmail.com>
2023-03-06 19:16:19 +0100

    add institute & tech prop

--- src/component/Text.tsx
+++ src/component/Text.tsx
@@ -5,13 +5,15 @@ interface IText {
   content: {
     slug: string;
     name: string;
+    institute?: string;
+    tech: string[];
     year: number;
     showYear: Boolean;
     end?: number | undefined;
     hidden?: Boolean | undefined;
   };
   y: number;
-  verticalPosition?: number | undefined;
+  horisontalPosition?: number | undefined;
   i?: number;
 }
 
@@ -30,11 +32,42 @@ const Text: React.FC<IText> = ({
   textColor,
   content,
   y,
-  verticalPosition
-
+  horisontalPosition
 }) => {
+
+
+  const Tech = () => {
+    return (
+      <>
+        {
+          content.tech.map((tech: string, i: number) => {
+            return (
+              <>
+                <tspan flood-color="#f00">{tech} </tspan>
+              </>
+            );
+          })
+        }
+      </>
+    );
+  }
+
+
   return (
-    <text x={verticalPosition && verticalPosition + 150} y={y} fill={content.hidden ? '#777' : textColor}>{content.name}</text>
+    <>
+      {/* Name */}
+      <text x={horisontalPosition && horisontalPosition + 150} y={y} fill={content.hidden ? '#777' : textColor}>
+        <tspan fontWeight="500">{content.name}</tspan>
+        {/* Institute */}
+        {content.institute && (
+          <tspan fontWeight="200"> – {content.institute}</tspan>
+        )}
+
+      </text>
+      {content.tech && (
+        <text fontSize="12" x={horisontalPosition && horisontalPosition + 150} y={y + 18} fill={content.hidden ? '#777' : textColor}>{<Tech />}</text>
+      )}
+    </>
   );
 };
 
