843ff6a65a915e528cd2117741caaa8181f7b97b
gabjuh <jjjjgabesz@gmail.com>
2023-03-06 10:20:48 +0100

    cleared up, get openBranchIndexes as prop

--- src/component/Branch.tsx
+++ src/component/Branch.tsx
@@ -10,9 +10,11 @@ interface IBranch {
   color: string | undefined,
   strokeWidth: number,
   pos: number[],
-  jumpToLevel: number;
-  heightIfNotEndedYet: number | null;
+  level: number;
+  heightTillTop: number | null;
+  levelDistanceReduction: number;
   open: string | undefined;
+  openBranchIndexes: number[] | undefined;
 }
 
 const Branch: React.FC<IBranch> = ({
@@ -23,48 +25,86 @@ const Branch: React.FC<IBranch> = ({
   color,
   strokeWidth,
   pos,
-  jumpToLevel,
-  heightIfNotEndedYet,
+  level,
+  heightTillTop,
+  levelDistanceReduction,
   open,
+  openBranchIndexes
 }) => {
 
+  // DRAWING ELEMENTS
+  // Curve 90degs from the bottom to the right
+  const curveBottomToRight: string = `
+    q 0 -${size} ${size} -${size}
+  `;
 
-  // The first line of code turns to the branch 90deg to the right, the third one 90deg to the left. The code in the middle makes it longer between, if jumpToLevel > 0
-  const bendRight: string = `
-   q 0 -${size} ${size} -${size}
-   ${jumpToLevel ? `l ${((1 - (jumpToLevel * .15)) * size + size) * (jumpToLevel)} 0` : ''}
-   q ${size} 0 ${size} -${size}
- `;
+  // Curve 90degs from the left to the top
+  const curveLeftToTop: string = `
+    q ${size} 0 ${size} -${size}
+  `;
 
-  // Same as above, but with different signs and direction
-  const bendLeft: string = `
-   q 0 -${size} -${size} -${size}
-   ${jumpToLevel ? `l -${((1 - (jumpToLevel * .15)) * size + size) * (jumpToLevel)} 0` : ''}
-   q -${size} 0 -${size} -${size}
-   `;
+  // Curve 90degs from the bottom to the left
+  const curveBottomToLeft: string = `
+    q 0 -${size} -${size} -${size}
+  `;
 
+  // Curve 90degs from the right to the top
+  const curveRightToTop: string = `
+    q -${size} 0 -${size} -${size}
+  `;
+
+  // Horisontal line to the left (sign = '-') or to the right (sign = ''),
+  // length between the curves is based on the level value
+  const straightLineHorisontal = (sign: string): string => `
+    ${level ? `l ${sign}${((1 - (level * levelDistanceReduction)) * size + size) * (level)} 0` : ''}
+  `;
+
+  // Vertical line from the bottom to the bottom in the given height
+  const straightLineVertical: string = `
+    l 0 ${height ? (height * step + step) * 1 : step}
+  `;
+
+  // Draw a curve to the right
+  // starting part of a right-side-branch
+  // endinging part of a left-side-branch
+  const curveRight: string = `
+    ${curveBottomToRight}
+    ${straightLineHorisontal('')}
+    ${curveLeftToTop}
+  `;
+
+  // Draw a curve to the left
+  // starting part of a left-side-branch
+  // endinging part of a right-side-branch
+  const curveLeft: string = `
+    ${curveBottomToLeft}
+    ${straightLineHorisontal('-')}
+    ${curveRightToTop}
+  `;
+
+  // LOGIC
   // Leave the end open to join it with the next branch, that no not have a start, if open set to 'end'
-  const leaveOpenOrClose = (bendString: string) =>
+  const leaveOpenOrClose = (curveString: string) =>
     open === 'end' ?
       `l 0 -${step * .75}` :
-      bendString;
+      curveString;
 
   // Turn to the right if side is right,
   // if false, it is the left side, turn to the left
-  const bendToDirection = () =>
+  const curveToDirection = () =>
     side === 'right' ?
-      leaveOpenOrClose(open !== 'end' ? bendLeft : '') :
-      leaveOpenOrClose(open !== 'end' ? bendRight : '');
+      leaveOpenOrClose(open !== 'end' ? curveLeft : '') :
+      leaveOpenOrClose(open !== 'end' ? curveRight : '');
 
   // Merge it to the timeline if branch has a finishing point
   const ifEnds: string = `
-    ${`l 0 ${height ? (height * step + step) * 1 : step}`}
-    ${bendToDirection()}
+    ${straightLineVertical}
+    ${curveToDirection()}
     `;
 
   // If branch has not a finishing point, so it is still active, it goes up to the top based on the remaining place to the top
   const lineIfNotEndedYet: string = `
-    l 0 -${heightIfNotEndedYet ? heightIfNotEndedYet * step + size * 1.3 : step}
+    l 0 -${heightTillTop ? heightTillTop * step + size * 1.3 : step}
  `;
 
   //  Original starting point from the timeline
@@ -74,7 +114,7 @@ const Branch: React.FC<IBranch> = ({
   const posContinuingPrevBranchLeft = `M ${pos[0] - size * 2} ${pos[1] + size}`;
 
   // Changed position for continuing a previous branch on the right side
-  const posContinuingPrevBranchRight = `M ${pos[0] + size * 2} ${pos[1] + size}`;
+  const posContinuingPrevBranchRight = `M ${pos[0] + (size * 2) * (level + 1)} ${pos[1] + size}`;
 
   // If open set to 'start', set position on the right or left side,
   // if open is not start, give the original starting point
@@ -85,21 +125,25 @@ const Branch: React.FC<IBranch> = ({
         posContinuingPrevBranchRight :
       posStartingFromTimeline;
 
+  const lineLengthToJoinOpenedBranches = openBranchIndexes ? openBranchIndexes[1] - openBranchIndexes[0] : 0;
+  console.log(openBranchIndexes);
+
   return (
     <>
       <path
         d={`
           ${getPosition()}
-          ${side === 'right' ? open !== 'start' ? bendRight : `q 0 0 0 -${step - size}` : open !== 'start' ? bendLeft : `q 0 0 0 -${step - size}`}
-          ${heightIfNotEndedYet ? lineIfNotEndedYet : ifEnds}
+          ${side === 'right' ? open !== 'start' ? curveRight : `q 0 0 0 -${step - size}` : open !== 'start' ? curveLeft : `q 0 0 0 -${step - size}`}
+          ${heightTillTop ? lineIfNotEndedYet :
+            openBranchIndexes ? `q 0 0 0 -${(step - size) * lineLengthToJoinOpenedBranches}` : ifEnds}
         `}
         stroke={color ? color : 'lightgray'}
         strokeWidth={strokeWidth}
         fill="none"
       />
 
-      {/* Point at the very end of the line, if it still active */}
-      {heightIfNotEndedYet && (
+      {/* Point at the very end of the line, if branch still active */}
+      {heightTillTop && (
         <Point
           color={color}
           isMajor={true}
