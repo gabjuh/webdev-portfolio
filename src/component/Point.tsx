38368335e50696f33b3684b7c01cd16b690b81b6
gabjuh <jjjjgabesz@gmail.com>
2023-03-06 19:17:21 +0100

    made horisontal pos dynamic

--- src/component/Point.tsx
+++ src/component/Point.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
+import themes from '../themes.json';
 
 interface IPoint {
-  color: string | undefined;
-  isMajor?: Boolean;
-  bgColor: string;
   pos: number[];
   size: number;
   strokeWidth: number;
+  color: string | undefined;
+  bgColor: string;
+  side?: string | undefined;
+  level?: number | undefined;
+  levelDistanceReduction: number;
+  branchWidth?: number;
+  isMajor?: Boolean;
+  isStillActive?: Boolean;
 }
 
 const Point: React.FC<IPoint> = ({
-  color,
-  isMajor,
-  bgColor,
   pos,
   size,
   strokeWidth,
+  color,
+  bgColor,
+  side,
+  level,
+  levelDistanceReduction,
+  branchWidth,
+  isMajor,
+  isStillActive
 }) => {
 
+  const getCx = () => {
+    if (!isStillActive) {
+      return side === 'left' ?
+        pos[0] - branchWidth * (level ? level * 2 : 0) :
+        pos[0] + branchWidth * (level ? level * 2 : 0);
+    } else {
+      return pos[0];
+    }
+
+  };
+
   return (
-    <g stroke={color} strokeWidth={isMajor ? strokeWidth : 0} fill={isMajor ? bgColor : color} >
-      <circle cx={pos[0]} cy={pos[1]} r={size} />
+    <g stroke={side ? themes[0].timeline : color} strokeWidth={isMajor ? strokeWidth : 0} fill={isMajor ? bgColor : color} >
+      <circle cx={getCx()} cy={pos[1]} r={size} />
     </g>
   );
 };
