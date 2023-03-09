547f41cacd4c81682e93d3c1c63da6fcf1558dca
gabjuh <jjjjgabesz@gmail.com>
2023-03-09 15:43:17 +0100

    change titles' position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <div className="hero-content flex-col">
         <img src={profil_img} className="max-w-[270px] h-[220px] w-[253px] rounded-xl shadow-md z-0" />
         <div>
-          <h1 className="text-4xl font-bold translate-y-4">Gábor Juhász</h1>
-          <p className="py-6 text-xl translate-y-6">Junior React-Frontend Entwickler</p>
+          <h1 className="text-xl font-bold translate-y-1">Gábor Juhász</h1>
+          <p className="pt-2 text-md translate-y-0">Junior React-Frontend Entwickler</p>
           <a className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44] mt-7">Kontakt</a>
 
         </div>
