057b4e8db8302e8bd7489f55728ef15da8218e50
gabjuh <jjjjgabesz@gmail.com>
2023-03-13 08:32:06 +0100

    change position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import profil_img from '../assets/profile2.jpg';
 
 const Hero = () => {
   return (
-    <div className="absolute left-[775px] -top-[1rem]">
+    <div className="translate-y-[60px]">
       <div className="hero-content flex-col">
-        <img src={profil_img} className="max-w-[270px] h-[220px] w-[253px] rounded-xl shadow-md z-0" />
+        <img src={profil_img} className="max-w-[270px] h-[220px] w-[265px] ml-8 rounded-xl shadow-md z-0" />
         <div>
           <h1 className="text-xl font-bold translate-y-1">Gábor Juhász</h1>
           <p className="pt-2 text-md translate-y-0">Junior React-Frontend Entwickler</p>
-          <a className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44] mt-7">Kontakt</a>
+          <a className="btn bg-transparent !text-[#444] !hover:text-white hover:bg-[#6dbc44] mt-7">Kontakt</a>
 
         </div>
       </div>
