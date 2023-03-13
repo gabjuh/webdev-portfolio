0d3435fe9f711aa228966f929761ec85824edbb2
gabjuh <jjjjgabesz@gmail.com>
2023-03-13 08:24:52 +0100

    changed to dynamic

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,18 +1,53 @@
 import React, { useState } from 'react';
 
-const Buttons = () => {
+interface IButton {
+  label: string;
+  isActive: boolean;
+  onClick: () => void;
+}
 
-  const [selectedButton, setSelectedButton] = useState();
+const Button: React.FC<IButton> = ({
+  label,
+  isActive,
+  onClick
+}) => {
+  return (
+    <button className={isActive ? 'btn btn-active' : 'btn'} onClick={onClick}>
+      {label}
+    </button>
+  )
+}
+
+const Buttons: React.FC = () => {
+  const [selectedButton, setSelectedButton] = useState<string>('Jobs');
+
+  const handleButtonClick = (label: string) => {
+    setSelectedButton(label);
+  }
+
+  const buttons = [
+    { label: 'Alles', value: 'alles'},
+    { label: 'Schule', value: 'schule'},
+    { label: 'Universität', value: 'universität'},
+    { label: 'Jobs', value: 'jobs'},
+    { label: 'Musik', value: 'musik'},
+    { label: 'IT', value: 'it'},
+    { label: 'Privat', value: 'privat'},
+  ]
 
   return (
-    <div className="btn-group ">
-      <button className={`btn bg-[#6dbc44] hover:bg-[#6dbc44]`}>All</button>
-      <button className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44]">IT</button>
-      <button className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44]">Music</button>
-      <button className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44]">Jobs</button>
-      <button className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44]">Schools</button>
-      <button className="btn bg-transparent text-[#444] hover:text-white hover:bg-[#6dbc44]">Private</button>
-    </div>
+    <>
+      <div className="btn-group">
+        {buttons.map(button => (
+          <Button 
+            key={button.value}
+            label={button.label}
+            isActive={selectedButton === button.label}
+            onClick={() => handleButtonClick(button.label)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
