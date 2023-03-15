8a9b9a09c5f4e6f1d867328cf3fd05703c43ba2e
gabjuh <jjjjgabesz@gmail.com>
2023-03-15 11:18:51 +0100

    add IYear interface

--- src/interfaces/Text.ts
+++ src/interfaces/Text.ts
@@ -1,9 +1,17 @@
 import { IContent } from './Tree';
 
+export interface IYear {
+  textColor: string;
+  content: IContent;
+  y: number;
+  i?: number;
+}
+
 export interface IText {
   textColor: string;
   content: IContent;
   y: number;
   horisontalPosition?: number;
   i?: number;
+  categoryColor: string;
 }
