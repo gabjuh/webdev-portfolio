a36b7325b4ac7e142ac2d3a543be934d75a90236
gabjuh <jjjjgabesz@gmail.com>
2023-03-16 14:25:05 +0100

    set texts responsive

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -42,10 +42,10 @@ const Text: React.FC<IText> = ({
     return (        
       <>
         {/* Background rectsquare */}
-        <rect x={horisontalPosition && horisontalPosition + 150} y={y - 18} width="45" height="24" rx="8" ry="8" fill={categoryColor} />
+        <rect x={horisontalPosition && horisontalPosition + 130} y={y - 18} width="45" height="24" rx="8" ry="8" fill={categoryColor} />
 
         {/* Category text */}
-        <text x={horisontalPosition && horisontalPosition + 157} y={y - 2}>
+        <text x={horisontalPosition && horisontalPosition + 137} y={y - 2}>
           <tspan fontSize=".6rem" fill="#fff" >{content.label.toUpperCase()}</tspan>
         </text>
       </>
@@ -54,23 +54,23 @@ const Text: React.FC<IText> = ({
 
 
   return (
-    <>
+    <g>
       {/* Category */}
       {content.label && <Category />}
 
       {/* Name */}
-      <text x={horisontalPosition && horisontalPosition + 210} y={y} fill={content.hidden ? '#777' : textColor}>
-        <tspan fontWeight="500">{content.name}</tspan>
+      <text className="hidden md:block" x={horisontalPosition && horisontalPosition + 190} y={y} fill={content.hidden ? '#777' : textColor}>
+        <tspan fontWeight="400">{content.name}</tspan>
         {/* Institute */}
-        {content.institute && (
-          <tspan fontWeight="200"> – {content.institute}</tspan>
-        )}
+        {/* {content.institute && (
+          <tspan fontWeight="100" fontSize="13"> – {content.institute}</tspan>
+        )} */}
 
       </text>
-      {content.tech && (
+      {/* {content.tech && (
         <text fontSize="12" x={horisontalPosition && horisontalPosition + 210} y={y + 18} fill={content.hidden ? '#777' : textColor}>{<Tech />}</text>
-      )}
-    </>
+      )} */}
+    </g>
   );
 };
 
