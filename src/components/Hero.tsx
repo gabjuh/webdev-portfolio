f9f8af4b0617f866375b51ce099cca61edb2c095
gabjuh <jjjjgabesz@gmail.com>
2023-03-16 10:25:00 +0100

    apply lg breakpoints

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import profil_img from '../assets/profile2.jpg';
 
 const Hero = () => {
   return (
-    <div className="translate-y-[60px]">
-      <div className="hero-content flex-col">
-        <img src={profil_img} className="max-w-[270px] h-[220px] w-[265px] ml-8 rounded-xl shadow-md z-0" />
-        <div>
+    <div className="lg:translate-y-[60px]">
+      <div className="hero-content lg:flex-col flex-row-reverse mb-[0px]">
+        <img src={profil_img} className="max-w-[270px] h-[220px] w-[265px] ml-8 rounded-xl shadow-md" />
+        <div className="text-right lg:text-left">
           <h1 className="text-xl font-bold translate-y-1">Gábor Juhász</h1>
           <p className="pt-2 text-md translate-y-0">Junior React-Frontend Entwickler</p>
-          <a className="btn bg-transparent !text-[#444] !hover:text-white hover:bg-[#6dbc44] mt-7">Kontakt</a>
+          <a className="btn bg-transparent hover:bg-[#009f81] text-[#000] hover:text-[#fff] mt-7">Kontakt</a>
 
         </div>
       </div>
