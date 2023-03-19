6e06a1fffbe8d706c27cf8c81b38bc7a55e65a46
gabjuh <jjjjgabesz@gmail.com>
2023-03-19 08:23:01 +0100

    state lifted up, add popup

--- src/components/CvTree.tsx
+++ src/components/CvTree.tsx
@@ -3,6 +3,7 @@ import Branch from './Branch';
 import Timeline from './Timeline';
 import Point from './Point';
 import Button from './Button';
+import Popup from './Popup';
 import { Year, Text } from './Text';
 import raw_data from '../cv_gj2.json';
 import raw_data_test from '../cv_test.json';
@@ -15,12 +16,10 @@ interface ICvTree {
   textColor: string;
 }
 
-interface IFilter {
-  filter?: 'all' | 'school' | 'university' | 'job' | 'music' | 'it' | 'private';
-}
+type IFilter = 'all' | 'school' | 'uni' | 'job' | 'music' | 'it' | 'private';
 
 interface ICategories {
-  label: string;
+  buttonLabel: string;
   value: string;
 }
 
@@ -75,11 +74,12 @@ const CvTree: React.FC<ICvTree> = ({
 
     if (content.end && endIndex === -1) {
 
-      // If the year of the fini^shing of a project is not a starting point of another (so it would not be shown), it will be added without any additional text.
+      // If the year of the finishing of a project is not a starting point of another (so it would not be shown), it will be added without any additional text.
       items.push({
         content: {
           slug: `generated_slug_${i}`,
-          label: '',
+          typeOfActivity: '',
+          categories: '',
           institute: '',
           name: '',
           year: content.end,
@@ -104,14 +104,23 @@ const CvTree: React.FC<ICvTree> = ({
     }
   });
 
-  const filter = undefined;
+  const [filter, setFilter] = useState<IFilter>('all');
 
+  const getColor = (item: IItem, filter: IFilter) => {
+    const category: string | string[] = item.content.categories
 
-  const getColor = (item: IItem, filter?: IFilter) => {
-
-    if (filter && filter !== item.content.label) {
-      return '#ccc';
+    if (filter !== 'all') {
+      if (Array.isArray(category)) {
+        if (!category.includes(filter)) {
+          return '#ccc';
+        }
+      } else {
+        if (category !== filter) {
+          return '#ccc';
+        }
+      }
     }
+
 
     let color = '';
 
@@ -132,185 +141,215 @@ const CvTree: React.FC<ICvTree> = ({
     return color;
   };
 
-  const [selectedButton, setSelectedButton] = useState<string>('Jobs');
+  const [selectedButton, setSelectedButton] = useState<IFilter>('all');
 
-  const handleButtonClick = (label: string) => {
-    setSelectedButton(label);
+  useEffect(() => {
+    setFilter(selectedButton);
+  }, [selectedButton]);
+
+  const handleButtonClick = (value: IFilter) => {
+    setSelectedButton(value);
+  };
+
+  const [showPopup, setShowPopup] = useState<string | undefined>();
+
+  const handleOnClickPopup = (e: React.MouseEvent<SVGTextElement, MouseEvent>) => {
+    setShowPopup(e.currentTarget.id);
   };
 
   return (
     <>
-      <div className="btn-categories flex flex-wrap sm:w-[450px] mt-24 mx-auto">
-        {categories.map(button => (
-          <Button
-            key={button.value}
-            label={button.label}
-            isActive={selectedButton === button.label}
-            onClick={() => handleButtonClick(button.label)}
-          />
-        ))}
-      </div>
-      <div
-        className={`relative mx-auto w-[390px] md:-translate-x-[100px]`}
-        style={{
-          // width: `${svgWidth}px`
-        }}
-      >
-        <div className="mx-auto w-[100%]">
-        </div>
-        <svg
-          height={svgHeight}
-          width={svgWidth}
-          className={`
-          w-[100vw]
-          relative
-          box-content
-        `}
+      <div className="overflow-hidden">
+        {/* Buttons */}
+        <div
+          className="btn-categories flex flex-wrap sm:w-[500px] mt-24 mx-auto"
+          style={{
+            position: "-webkit-sticky",
+            top: 0,
+          }}
         >
-          <g transform="translate(0, 23)">
+          {categories.map((category, i) => (
+            <React.Fragment key={`button_${i}`}>
+              <Button
+                key={category.value}
+                label={category.buttonLabel}
+                isActive={selectedButton === category.value}
+                onClick={() => handleButtonClick(category.value as IFilter)}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+        <div className={`relative mx-auto w-[390px] md:-translate-x-[100px]`} >
+          <div className="mx-auto w-[100%]">
+          </div>
+          <svg
+            height={svgHeight}
+            width={svgWidth}
+            className={`
+            w-[100vw]
+            relative
+            box-content
+          `}
+          >
+            <g transform="translate(0, 23)">
 
 
-            {/* Loop of items */}
-            {items.map((_, i, a) => {
-              const index = a.length - 1 - i;
-              const item = items[index];
-              const content = item.content;
-              const layout = item.layout;
-              const side: string = layout ? layout.side : 'left';
-              const yPos = step * (i + 1) + 6;
-              const end = layout?.end
-              const color = getColor(item, filter)
+              {/* Loop of items */}
+              {items.map((_, i, a) => {
+                const index = a.length - 1 - i;
+                const item = items[index];
+                const content = item.content;
+                const layout = item.layout;
+                const side: string = layout ? layout.side : 'left';
+                const yPos = step * (i + 1) + 6;
+                const end = layout?.end;
+                const color = getColor(item, filter)
 
-              // LOGIC TO GET THE PROPER HEIGHT OF AN UNFINISHED BRANCH
-              // Find the year object index, where the end year is
-              const endYearIndex = sortedItems.findIndex(obj => content.end && content.end === obj.content.year);
+                // LOGIC TO GET THE PROPER HEIGHT OF AN UNFINISHED BRANCH
+                // Find the year object index, where the end year is
+                const endYearIndex = sortedItems.findIndex(obj => content.end && content.end === obj.content.year);
 
-              // Get the difference - not in years but in index-numbers!
-              const startEndDiff = endYearIndex && endYearIndex > -1 ? index - endYearIndex : 1;
+                // Get the difference - not in years but in index-numbers!
+                const startEndDiff = endYearIndex && endYearIndex > -1 ? index - endYearIndex : 1;
 
-              // Get index, if item has no end point but still has a branch.
-              const indexIfNotEndedYet = layout && !content.end && content.name ? index : null;
+                // Get index, if item has no end point but still has a branch.
+                const indexIfNotEndedYet = layout && !content.end && content.name ? index : null;
 
-              // Extract not ended index from the length of sortedItems
-              const heightTillTop: number | null = indexIfNotEndedYet && indexIfNotEndedYet > 0 ? sortedItems.length - indexIfNotEndedYet -1 : null;
+                // Extract not ended index from the length of sortedItems
+                const heightTillTop: number | null = indexIfNotEndedYet && indexIfNotEndedYet > 0 ? sortedItems.length - indexIfNotEndedYet - 1 : null;
 
-              // LOGIC TO GET THE DISTANCE OF AN OPEN ENDED AND OPEN STARTED BRANCH
-              let openBranchIndexes: number[] | undefined = undefined;
+                // LOGIC TO GET THE DISTANCE OF AN OPEN ENDED AND OPEN STARTED BRANCH
+                let openBranchIndexes: number[] | undefined = undefined;
 
-              if (item.content.end && item.layout?.open === 'end') {
-                const arr = [];
-                const indexOpenEnded = sortedItems.findIndex(el => el === item);
-                arr.push(indexOpenEnded);
+                if (item.content.end && item.layout?.open === 'end') {
+                  const arr = [];
+                  const indexOpenEnded = sortedItems.findIndex(el => el === item);
+                  arr.push(indexOpenEnded);
 
-                const match = sortedItems.filter(el =>
-                  item.layout?.side === el.layout?.side &&
-                  item.layout?.level === el.layout?.level &&
-                  el.layout?.open === 'start'
-                );
+                  const match = sortedItems.filter(el =>
+                    item.layout?.side === el.layout?.side &&
+                    item.layout?.level === el.layout?.level &&
+                    el.layout?.open === 'start'
+                  );
 
-                const indexOpenStarted = sortedItems.findIndex(el => el === match[0]);
+                  const indexOpenStarted = sortedItems.findIndex(el => el === match[0]);
 
-                indexOpenStarted && arr.push(indexOpenStarted);
+                  indexOpenStarted && arr.push(indexOpenStarted);
 
-                openBranchIndexes = arr;
-              }
+                  openBranchIndexes = arr;
+                }
 
-              return (
-                <React.Fragment key={i}>
+                return (
+                  <React.Fragment key={`branch_${i}`}>
 
-                  {/* Branch */}
-                  {layout &&
-                    <Branch
-                      height={startEndDiff - 0.5}
-                      step={step}
-                      size={size}
-                      side={side}
-                    color={color}
-                      bgColor={general.bgColor}
-                      strokeWidth={strokeWidth}
-                      pos={[horisontalPosition, step * (i + 1) + step * .58]}
-                      heightTillTop={heightTillTop}
-                      level={layout?.startingLevel}
-                      levelDistanceReduction={general.levelDistanceReduction}
-                      open={layout.open}
-                      openBranchIndexes={openBranchIndexes ? openBranchIndexes : undefined}
-                      newBranchOn={layout.newBranchOn}
-                      pointStrokeWidth={pointStrokeWidth}
-                      pointSize={pointSize}
-                      end={end}
-                    canceled={layout.canceled}
+                    {/* Branch */}
+                    {layout &&
+                      <Branch
+                        height={startEndDiff - 0.5}
+                        step={step}
+                        size={size}
+                        side={side}
+                        color={color}
+                        bgColor={general.bgColor}
+                        strokeWidth={strokeWidth}
+                        pos={[horisontalPosition, step * (i + 1) + step * .58]}
+                        heightTillTop={heightTillTop}
+                        level={layout?.startingLevel}
+                        levelDistanceReduction={general.levelDistanceReduction}
+                        open={layout.open}
+                        openBranchIndexes={openBranchIndexes ? openBranchIndexes : undefined}
+                        newBranchOn={layout.newBranchOn}
+                        pointStrokeWidth={pointStrokeWidth}
+                        pointSize={pointSize}
+                        end={end}
+                        canceled={layout.canceled}
+                      />
+                    }
+
+                    {/* Year */}
+                    <Year
+                      content={content}
+                      textColor={textColor}
+                      y={yPos}
+                      i={index}
                     />
-                  }
 
-                  {/* Year */}
-                  <Year
-                    content={content}
-                    textColor={textColor}
-                    y={yPos}
-                    i={index}
-                  />
+                    {/* Name */}
+                    <Text
+                      content={content}
+                      textColor={color}
+                      y={yPos}
+                      horisontalPosition={horisontalPosition}
+                      categoryColor={color}
+                      onClick={handleOnClickPopup}
+                      showPopup={{ showPopup, setShowPopup }}
+                    />
 
-                  {/* Name */}
-                  <Text
-                    content={content}
-                    textColor={textColor}
-                    y={yPos}
-                    horisontalPosition={horisontalPosition}
-                    categoryColor={color}
-                  />
+                  </React.Fragment>
+                );
+              })}
 
-                </React.Fragment>
-              );
-            })}
+              {/* Timeline */}
+              <Timeline
+                height={timelineHeight}
+                color={timeline.color}
+                strokeWidth={strokeWidth}
+                startPos={timelineStartPos}
+                isTimeline={true}
+                horisontalPosition={horisontalPosition}
+              />
 
-            {/* Timeline */}
-            <Timeline
-              height={timelineHeight}
-              color={timeline.color}
-              strokeWidth={strokeWidth}
-              startPos={timelineStartPos}
-              isTimeline={true}
-              horisontalPosition={horisontalPosition}
-            />
+              {/* Point at the very end  */}
+              <Point
+                pos={[horisontalPosition, 0]}
+                pointSize={pointSize}
+                strokeWidth={pointStrokeWidth}
+                color={timeline.color}
+                bgColor={bgColor}
+                isMajor={true}
+                levelDistanceReduction={general.levelDistanceReduction}
+                branchWidth={general.size}
+              />
 
-            {/* Point at the very end  */}
-            <Point
-              pos={[horisontalPosition, 0]}
-              pointSize={pointSize}
-              strokeWidth={pointStrokeWidth}
-              color={timeline.color}
-              bgColor={bgColor}
-              isMajor={true}
-              levelDistanceReduction={general.levelDistanceReduction}
-              branchWidth={general.size}
-            />
-
-            {/* Points */}
-            {items.map((item, i, a) => {
-              const index = a.length - 1 - i;
-              return (
-                <React.Fragment key={i}>
-                  <Point
-                    pos={[general.horisontalPosition, step * (index + 1)]}
-                    pointSize={pointSize}
-                    strokeWidth={pointStrokeWidth}
-                    color={getColor(item, filter)}
-                    bgColor={bgColor}
-                    isMajor={item.content.isMajor}
-                    side={item.layout?.side}
-                    level={item.layout?.level}
-                    levelDistanceReduction={general.levelDistanceReduction}
-                    branchWidth={general.size}
-                  />
-                </React.Fragment>
-              );
-            })}
-
-          </g>
-        </svg>
+              {/* Points */}
+              {items.map((item, i, a) => {
+                const index = a.length - 1 - i;
+                const color = getColor(item, filter)
+                return (
+                  <>
+                    <React.Fragment key={`point_${i}`}>
+                      <Point
+                        pos={[general.horisontalPosition, step * (index + 1)]}
+                        pointSize={pointSize}
+                        strokeWidth={pointStrokeWidth}
+                        color={color}
+                        bgColor={bgColor}
+                        isMajor={item.content.isMajor}
+                        side={item.layout?.side}
+                        level={item.layout?.level}
+                        levelDistanceReduction={general.levelDistanceReduction}
+                        branchWidth={general.size}
+                      />
+                    </React.Fragment>
+                    <React.Fragment key={`popup_${i}`}>
+                      <Popup
+                        color={color}
+                        content={item.content}
+                        verticalPosition={step * (index + 1) + 6}
+                        horisontalPosition={horisontalPosition}
+                        showPopup={showPopup}
+                        setShowPopup={setShowPopup}
+                      />
+                    </React.Fragment>
+                  </>
+                );
+              })}
+            </g>
+          </svg>
+        </div>
       </div>
     </>
   );
 };
 
-export default CvTree;;
+export default CvTree;
