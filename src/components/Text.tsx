5eb624af582140370ac166441a5aadc2c2588a1a
gabjuh <jjjjgabesz@gmail.com>
2023-03-19 08:18:49 +0100

    remove category and tech texts, add mouseEvents to name

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IText, IYear } from '../interfaces/Text';
 
 const Year: React.FC<IYear> = ({
@@ -20,9 +20,9 @@ const Text: React.FC<IText> = ({
   content,
   y,
   horisontalPosition,
-  categoryColor
+  categoryColor,
+  onClick,
 }) => {
-
 
   const Tech = () => {
     return (
@@ -38,38 +38,39 @@ const Text: React.FC<IText> = ({
     );
   }
 
-  const Category = () => {
-    return (        
-      <>
-        {/* Background rectsquare */}
-        <rect x={horisontalPosition && horisontalPosition + 130} y={y - 18} width="45" height="24" rx="8" ry="8" fill={categoryColor} />
+  const handleMouseEnter = (e: React.MouseEvent<SVGTextElement, MouseEvent>) => {
+    e.currentTarget.setAttribute('fill', '#333');
+  };
 
-        {/* Category text */}
-        <text x={horisontalPosition && horisontalPosition + 137} y={y - 2}>
-          <tspan fontSize=".6rem" fill="#fff" >{content.label.toUpperCase()}</tspan>
-        </text>
-      </>
-    )
-  }
-
+  const handleMouseLeave = (e: React.MouseEvent<SVGTextElement, MouseEvent>) => {
+    const el = e.currentTarget;
+    setTimeout(() => {
+      el.setAttribute('fill', content.hidden ? '#777' : textColor);
+    }, 800);
+  };
 
   return (
     <g>
-      {/* Category */}
-      {content.label && <Category />}
 
       {/* Name */}
-      <text className="hidden md:block" x={horisontalPosition && horisontalPosition + 190} y={y} fill={content.hidden ? '#777' : textColor}>
-        <tspan fontWeight="400">{content.name}</tspan>
+      <text
+        id={content.slug}
+        className="hidden md:block transition-all duration-[400ms] ease-in-out cursor-pointer"
+        onMouseEnter={e => handleMouseEnter(e)}
+        onMouseLeave={e => handleMouseLeave(e)}
+        onClick={e => onClick(e)}
+        fill={content.hidden ? '#777' : textColor}
+        x={horisontalPosition && horisontalPosition + 140}
+        y={y}
+      >
+        <tspan fontWeight="450">{content.name}</tspan>
+        {/* ico */}
         {/* Institute */}
         {/* {content.institute && (
-          <tspan fontWeight="100" fontSize="13"> – {content.institute}</tspan>
+          <tspan fill="#333" fontWeight="100" fontSize="13"> – {content.institute}</tspan>
         )} */}
 
       </text>
-      {/* {content.tech && (
-        <text fontSize="12" x={horisontalPosition && horisontalPosition + 210} y={y + 18} fill={content.hidden ? '#777' : textColor}>{<Tech />}</text>
-      )} */}
     </g>
   );
 };
