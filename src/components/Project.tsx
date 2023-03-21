e4371d204659a2560d48cb4efde460004ee12c6f
gabjuh <jjjjgabesz@gmail.com>
2023-03-21 10:00:00 +0100

    create Project

---
+++ src/components/Project.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { images } from './images';
+
+interface IProject {
+  key: number;
+  title: string;
+  description: string;
+  stack: string[];
+  img: string;
+}
+
+const Project: React.FC<IProject> = ({
+  key,
+  title,
+  description,
+  stack,
+  img
+}) => {
+  console.log(img);
+  return (
+    <>
+      {/* img */}
+      <div className="md:cols-1">
+        <img className="lg:w-[400px]" src={images[img]} alt="" />
+      </div>
+      <div className="md:cols-1 md:text-left pr-5 my-10">
+
+        {/* Title */}
+        <h2 className="text-3xl font-bold">{title}</h2>
+
+        {/* Description */}
+        <p className="text-lg text-gray-500 mt-7">
+          {description}
+        </p>
+
+        {/* Stack */}
+        <div className="flex flex-wrap gap-2 mt-4">
+          {stack.map((item, index) => (
+            <span key={index} className="text-sm bg-gray-200 text-gray-700 rounded-full px-3 py-1">
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Project;
