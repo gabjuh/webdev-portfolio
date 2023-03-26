07fdd05534740ce1251847b1e5e04e962669b854
gabjuh <jjjjgabesz@gmail.com>
2023-03-26 20:23:42 +0200

    create downButton in separate file

---
+++ src/data/stack/downButton.ts
@@ -0,0 +1,15 @@
+import downArrow from '../../assets/logos/down-arrow.svg';
+
+interface IDownButton {
+  index: number;
+  name: string;
+  img: string;
+}
+
+const downButton: IDownButton = {
+  "index": 88,
+  "name": 'down_arrow',
+  "img": downArrow
+};
+
+export default downButton;
