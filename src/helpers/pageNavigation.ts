b596958a23349c8faa4cac9fec610dd0f6238fac
gabjuh <jjjjgabesz@gmail.com>
2023-03-26 20:22:54 +0200

    create pageNavigation helpers file and add scrollToId

---
+++ src/helpers/pageNavigation.ts
@@ -0,0 +1,8 @@
+const scrollToId = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+export { scrollToId };
