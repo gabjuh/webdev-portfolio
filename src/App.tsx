c9862ed8501895108e1dc52b0fa89e9837dfee41
gabjuh <jjjjgabesz@gmail.com>
2023-03-26 20:25:48 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,19 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import CvTree from './components/CvTree';
-import Nav from './components/Nav';
-// import Buttons from './components/Buttons';
 import Stack from './components/Stack';
 import Footer from './components/Footer';
-import Projects from './components/Projects';
 import Chat from './components/Chat';
 
-const scrollToId = (id: string) => {
-  const element = document.getElementById(id);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
-  }
-}
+
 
 function App() {
-
-  const colors = ['#eee', '#000'];
-
-  const bgColor = colors[0];
-  const textColor = colors[1];
-
-  const html = document.querySelector('html') as HTMLElement;
-
-  html.style.backgroundColor = bgColor;
-  html.style.color = textColor;
 
   return (
     <div className="App">
       <div className="z-0">
-        <Stack
-          scrollToId={scrollToId}
-        />
-
-        {/* <Buttons
-        // categories={categories}
-        /> */}
-        <CvTree
-          bgColor={bgColor}
-          textColor={textColor}
-          scrollToId={scrollToId}
-        />
-        {/* <Projects /> */}
+        <Stack />
+        <CvTree />
         <Chat />
         <Footer />
       </div>
