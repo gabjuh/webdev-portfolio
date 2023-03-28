538b4339cfa6bfc77712c86a3cb82adc613568ee
gabjuh <jjjjgabesz@gmail.com>
2023-03-28 21:54:13 +0200

    add unique id-s for pattern with slug included, add comments

--- src/components/Ruler.tsx
+++ src/components/Ruler.tsx
@@ -4,43 +4,49 @@ interface IRuler {
   showUp: boolean;
   color?: string;
   startPos: number[];
+  slug?: string;
 }
 
 const Ruler: React.FC<IRuler> = ({
   showUp,
   color,
   startPos,
+  slug
 }) => {
   return (
     <>
+      {/* Define striped pattern Ruler with dynamic color */}
       <defs>
-        <pattern id="svg-stripes-pattern-11424" patternTransform="rotate(-45)" width="90%" height="8" x="0" y="0" patternUnits="userSpaceOnUse">
+        <pattern id={`svg-stripes-pattern-11424-${slug}-a`} patternTransform="rotate(-45)" width="90%" height="8" x="0" y="0" patternUnits="userSpaceOnUse">
           <animate attributeName="y" begin="0s" dur="1000s" from="5000px" to="0px" repeatCount="indefinite" fill="freeze"></animate>
           <g>
             <rect x="0" y="0" width="100%" height="4" fill={color} className="opacity-[.18]"></rect>
           </g>
         </pattern>
-        <pattern id="svg-stripes-pattern-11425" patternTransform="rotate(45)" width="90%" height="8" x="0" y="0" patternUnits="userSpaceOnUse">
+        <pattern id={`svg-stripes-pattern-11424-${slug}-b`} patternTransform="rotate(45)" width="90%" height="8" x="0" y="0" patternUnits="userSpaceOnUse">
           <animate attributeName="y" begin="0s" dur="1000s" from="5000px" to="0px" repeatCount="indefinite" fill="freeze"></animate>
           <g>
             <rect x="0" y="0" width="100%" height="4" fill={color} className="opacity-[.18]"></rect>
           </g>
         </pattern>
       </defs>
+      {/* Draw Ruler */}
       <g className={`${showUp ? 'visible' : 'invisible'} transition-all duration-300`}>
+        {/* Arrows' half part above */}
         <rect
           x={startPos[0]}
           y={startPos[1] + 1}
           width="700"
           height="7"
-          fill="url(#svg-stripes-pattern-11424)"
+          fill={`url(#svg-stripes-pattern-11424-${slug}-a)`}
         ></rect>
+        {/* Arrows' half part below */}
         <rect
           x={startPos[0]}
           y={startPos[1] - 6}
           width="700"
           height="7"
-          fill="url(#svg-stripes-pattern-11425)"
+          fill={`url(#svg-stripes-pattern-11424-${slug}-b)`}
         ></rect>
       </g>
     </>
