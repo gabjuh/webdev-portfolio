521c0ea633c2b04bd20671d6cdb804d20f516b48
gabjuh <jjjjgabesz@gmail.com>
2023-03-28 14:16:38 +0200

    add wrapper div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,24 +8,27 @@ interface IHero {
 
 const Hero: React.FC<IHero> = ({ scrollToId }) => {
   return (
-    <div className="relative lg:translate-y-[136px] h-[95vh] md:h-[100%]">
-      <div className="hero-content flex-col md:flex-row-reverse lg:flex-col">
-        <img src={profil_img} className="max-w-[270px] h-[228px] lg:ml-8 rounded-xl shadow-md brightness-[1.2]" />
-        <div className="text-center md:text-right lg:text-left">
-          <h1 className="text-xl font-bold translate-y-1">Gábor Juhász</h1>
-          <p className="pt-2 text-md translate-y-0">Junior React-Frontend Entwickler</p>
-          <a href="mailto:adgbe@yahoo.de" className="btn btn-sm bg-transparent hover:bg-[#2db6a1] text-[#000] text-xs px-5 hover:text-[#fff] mt-7 shadow-md border-[.5px]">Kontakt</a>
+    <div className="lg:col-span-2 col-span-8 lg:order-1 -order-1">
+      <div className="relative lg:translate-y-[136px] h-[95vh] md:h-[100%]">
+        <div className="hero-content flex-col md:flex-row-reverse lg:flex-col">
+          <img src={profil_img} className="max-w-[270px] h-[228px] lg:ml-8 rounded-xl shadow-md brightness-[1.2]" />
+          <div className="text-center md:text-right lg:text-left">
+            <h1 className="text-xl font-bold translate-y-1">Gábor Juhász</h1>
+            <p className="pt-2 text-md translate-y-0">Junior React-Frontend Entwickler</p>
+            <a href="mailto:adgbe@yahoo.de" className="btn btn-sm bg-transparent hover:bg-[#2db6a1] text-[#000] text-xs px-5 hover:text-[#fff] mt-7 shadow-md border-[.5px]">Kontakt</a>
+          </div>
+        </div>
+        <div className="visible md:invisible block absolute bottom-5 w-[100%] text-center">
+          <button
+            className="py-3 px-3 rounded-3xl shadow-md bg-[#28cbb7] transition-all duration-[150] hover:text-[#fff] hover:-translate-y-[.3rem] animate-bounce"
+            onMouseUp={() => scrollToId('cv')}
+          >
+            <img src={downArrow} alt="arrow" />
+          </button>
         </div>
       </div>
-      <div className="visible md:invisible block absolute bottom-5 w-[100%] text-center">
-        <button
-          className="py-3 px-3 rounded-3xl shadow-md bg-[#28cbb7] transition-all duration-[150] hover:text-[#fff] hover:-translate-y-[.3rem] animate-bounce"
-          onMouseUp={() => scrollToId('cv')}
-        >
-          <img src={downArrow} alt="arrow" />
-        </button>
-      </div>
     </div>
+
   );
 };
 
