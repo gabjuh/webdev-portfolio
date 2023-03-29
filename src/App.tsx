95547f147623926dcff5a8e6a5886f57c3307d39
gabjuh <jjjjgabesz@gmail.com>
2023-03-29 22:51:00 +0200

    import and set routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
 import './App.css';
 import CvTree from './components/CvTree';
 import Stack from './components/Stack';
@@ -8,12 +9,17 @@ import Chat from './components/Chat';
 function App() {
   return (
     <div className="App">
-      <div className="z-0">
-        <Stack />
-        <CvTree />
-        <Chat />
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <div className="z-0">
+            <Stack />
+            <CvTree />
+            <Chat />
+          </div>
+        } />
+        <Route path="/credits" element={<div>Credits</div>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
