c19d5263771f232ecd2bb44c1edb1c2431cae565
gabjuh <jjjjgabesz@gmail.com>
2023-03-29 22:52:31 +0200

    move interfaces to separate file

--- src/components/Branch.tsx
+++ src/components/Branch.tsx
@@ -1,29 +1,6 @@
 import React, { useState } from 'react';
 import Point from './Point';
-
-interface IBranch {
-  height: number | null,
-  step: number,
-  size: number,
-  side: string,
-  color?: string,
-  bgColor: string,
-  strokeWidth: number,
-  pos: number[],
-  level: number;
-  heightTillTop: number | null;
-  levelDistanceReduction: number;
-  open?: string;
-  openBranchIndexes?: number[];
-  newBranchOn?: {
-    start: number;
-    end?: number;
-  };
-  pointStrokeWidth: number;
-  pointSize: number;
-  end?: number;
-  canceled?: boolean;
-}
+import IBranch from '../interfaces/Branch';
 
 const Branch: React.FC<IBranch> = ({
   height,
