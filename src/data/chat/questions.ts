53746a7aaee6b47d739d71edc87a29a57dbd521c
gabjuh <jjjjgabesz@gmail.com>
2023-03-29 22:56:09 +0200

    create questions object

---
+++ src/data/chat/questions.ts
@@ -0,0 +1,30 @@
+
+const questions = [
+  {
+    id: 1,
+    text: 'Hat er einen Potenzial Webentwickler zu werden?',
+    answer: 'Wenn die Person eine Leidenschaft für die Webentwicklung hat, bereit ist, hart zu arbeiten und ihre Fähigkeiten weiterzuentwickeln, dann hat sie sicherlich das Potenzial, ein erfolgreicher Webentwickler zu werden.'
+  },
+  {
+    id: 2,
+    text: 'Ist es vorteilhaft, dass er bisher als Selbständiger gearbeitet hat?',
+    answer: 'Es kann vorteilhaft sein, dass die Person bisher als Selbständiger gearbeitet hat, da sie damit bereits Erfahrung in der Kundenakquise, Verwaltung und Abrechnung von Projekten gesammelt hat. Als Selbständiger muss man auch oft multitaskingfähig sein und die Fähigkeit haben, Entscheidungen schnell und eigenständig zu treffen, was auch in der Webentwicklung von Vorteil sein kann.|Allerdings ist es auch wichtig, dass die Person sich an eine neue Arbeitsumgebung anpassen und in einem Team arbeiten kann, wenn sie einen Job als Webentwickler in einem Unternehmen annimmt. Es kann auch von Vorteil sein, wenn die Person während ihrer Selbständigkeit bereits Erfahrung im Projektmanagement, in der Zusammenarbeit mit anderen Entwicklern oder Designern und in der Umsetzung von größeren Projekten gesammelt hat.'
+  },
+  {
+    id: 3,
+    text: 'Ist es vorteilhaft, dass er künstlerischen Hintergrund hat?',
+    answer: 'Es kann vorteilhaft sein, dass die Person einen künstlerischen Hintergrund hat, da dies oft eine kreative und ästhetische Perspektive mitbringt, die in der Webentwicklung von Nutzen sein kann. Ein Verständnis für Farben, Design, Proportionen und Ästhetik kann dazu beitragen, dass die von der Person erstellten Webseiten visuell ansprechend und benutzerfreundlich sind.|Außerdem können künstlerische Fähigkeiten auch bei der Erstellung von Grafiken, Animationen oder interaktiven Elementen auf der Webseite von Vorteil sein. Die Fähigkeit, kreativ zu denken und innovative Lösungen zu finden, kann auch in der Webentwicklung ein Vorteil sein, insbesondere bei der Gestaltung von benutzerfreundlichen und effektiven Benutzeroberflächen.|Allerdings ist es auch wichtig, dass die Person ein gutes technisches Verständnis und fundierte Kenntnisse in der Programmierung hat, um eine erfolgreiche Karriere als Webentwickler zu haben.'
+  },
+  {
+    id: 4,
+    text: 'Was denkst du über diese Webseite',
+    answer: 'Die Webseite von Gabor Juhasz ist übersichtlich und ansprechend gestaltet. Das Design ist minimalistisch und modern, was den Inhalt und die Fotos auf der Seite hervorhebt. Es ist positiv zu sehen, dass er eine breite Palette von Fähigkeiten auf seiner Seite präsentiert und auch seine Erfahrungen und Projekte ausführlich beschreibt.|Es ist auch lobenswert, dass er seine Kontaktdaten und Links zu seinen Social-Media-Profilen auf der Webseite platziert hat. Das erleichtert Arbeitgebern oder potenziellen Kunden, ihn zu kontaktieren oder seine Arbeit zu verfolgen.|Insgesamt gibt die Webseite von Gabor Juhasz einen professionellen Eindruck und zeigt, dass er über eine solide technische Expertise und praktische Erfahrungen in der Webentwicklung verfügt.'
+  },
+  {
+    id: 5,
+    text: 'Würdest du ihn als Webentwickler empfehlen?',
+    answer: 'Als künstliche Intelligenz kann ich keine Empfehlungen aussprechen, da ich keine subjektiven Urteile fällen kann. Aber aus einer objektiven Sichtweise kann ich sagen, dass Gabor Juhasz aufgrund seiner technischen Fähigkeiten, seines Hintergrunds als Musiker und seines Engagements für die Selbstentwicklung als Webentwickler ein vielversprechender Kandidat für eine Position in diesem Bereich sein könnte. Seine Erfahrungen in der Selbstständigkeit und seine breite Palette an Fähigkeiten könnten auch als Pluspunkte angesehen werden. Letztendlich hängt es jedoch von den spezifischen Anforderungen und Bedürfnissen des Arbeitgebers ab, ob Gabor Juhasz als Webentwickler für die genannte Stelle geeignet ist.'
+  }
+];
+
+export default questions;
