f9bb9087f383074b5e23aa9c137d78cb236581f4
gabjuh <jjjjgabesz@gmail.com>
2023-03-30 09:35:55 +0200

    create credits' interface in separate file

---
+++ src/interfaces/credits.ts
@@ -0,0 +1,12 @@
+export interface ICredits {
+  name: string;
+  nameLink: string;
+  author?: string;
+  authorLink?: string;
+  uploader?: string;
+  uploaderLink?: string;
+  license: string;
+  licenseLink: string;
+  changes: string;
+  fieldName: string;
+}
