aa82540aab72c12b6fc8e4c29b58af9dab7f1b87
gabjuh <jjjjgabesz@gmail.com>
2023-03-30 09:37:06 +0200

    create credits data object

---
+++ src/data/credits/credits.ts
@@ -0,0 +1,216 @@
+import { ICredits } from '../../interfaces/credits';
+
+const credits: ICredits[] = [
+  {
+    name: 'Photoshop SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/508752/photoshop',
+    author: 'Pictonic',
+    authorLink: 'https://www.svgrepo.com/author/Pictonic/',
+    license: 'CC Attribution License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#CC%20Attribution',
+    changes: 'none',
+    fieldName: 'Photoshop CC'
+  },
+  {
+    name: 'HTML SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/447394/html',
+    author: 'Wordpress',
+    authorLink: 'https://www.svgrepo.com/author/Wordpress/',
+    license: 'GPL License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#GPL',
+    changes: 'none',
+    fieldName: 'HTML5'
+  },
+  {
+    name: 'Css SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/104795/css',
+    uploader: 'SVG Repo',
+    uploaderLink: 'https://www.svgrepo.com/',
+    license: 'CC0 License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#CC0',
+    changes: 'none',
+    fieldName: 'CSS4'
+  },
+  {
+    name: 'TypeScript SVG Vector',
+    nameLink: 'https://www.svgviewer.dev/s/476576/typescript',
+    author: 'undefinedvscode-icons',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'Color changed to black',
+    fieldName: 'TypeScript'
+  },
+  {
+    name: 'React SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/358128/react',
+    author: 'Iconscout',
+    authorLink: 'https://www.svgrepo.com/author/Iconscout/',
+    license: 'Apache License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#Apache',
+    changes: 'none',
+    fieldName: 'React'
+  },
+  {
+    name: 'Tailwind Css SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/374118/tailwind',
+    author: 'vscode-icons',
+    authorLink: 'https://www.svgrepo.com/author/vscode-icons/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'Color changed to black',
+    fieldName: 'Tailwind CSS'
+  },
+  {
+    name: 'Git SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394173/git',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'git'
+  },
+  {
+    name: 'VS Code SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/452129/vs-code',
+    author: 'Garuda Technology',
+    authorLink: 'https://www.svgrepo.com/author/Garuda%20Technology/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'all colors changed to black',
+    fieldName: 'VSCode'
+  },
+  {
+    name: 'Js02 SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/508924/js02',
+    author: 'Pictonic',
+    authorLink: 'https://www.svgrepo.com/author/Pictonic/',
+    license: 'CC Attribution License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#CC%20Attribution',
+    changes: 'none',
+    fieldName: 'JavaScript'
+  },
+  {
+    name: 'Php02 SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/508936/php02',
+    author: 'Pictonic',
+    authorLink: 'https://www.svgrepo.com/author/Pictonic/',
+    license: 'CC Attribution License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#CC%20Attribution',
+    changes: 'none',
+    fieldName: 'PHP8'
+  },
+  {
+    name: 'Laravel SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394247/laravel',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Laravel 8'
+  },
+  {
+    name: 'Wordpress SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394568/wordpress',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Wordpress'
+  },
+  {
+    name: 'Joomla SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394234/joomla',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Joomla'
+  },
+  {
+    name: 'Sqlite SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/374094/sqlite',
+    author: 'Vscode Icons',
+    authorLink: 'https://www.svgrepo.com/collection/vscode-icons/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'all colors changed to black',
+    fieldName: 'SQLite'
+  },
+  {
+    name: 'Mysql SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394296/mysql',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'MySQL'
+  },
+  {
+    name: 'Nodejs SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394296/mysql',
+    author: 'etn-ccis',
+    authorLink: 'https://www.svgrepo.com/author/etn-ccis/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Node.JS'
+  },
+  {
+    name: 'Adobe Indesign SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/167696/adobe-indesign-logo',
+    uploader: 'SVG Repo',
+    uploaderLink: 'https://www.svgrepo.com/',
+    license: 'CC0 License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#CC0',
+    changes: 'none',
+    fieldName: 'InDesign CC'
+  },
+  {
+    name: 'Bootstrap Fill SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/378490/bootstrap-fill',
+    author: 'artcoholic',
+    authorLink: 'https://www.svgrepo.com/author/artcoholic/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Bootstrap 4'
+  },
+  {
+    name: 'Sass SVG Vector',
+    nameLink: 'https://www.svgrepo.com/svg/394408/sass',
+    author: 'Kenan Gudogan',
+    authorLink: 'https://www.svgrepo.com/author/Kenan%20Gundogan/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'Sass'
+  },
+  {
+    name: 'Copilot SVG Vector',
+    nameLink: 'https://icon-sets.iconify.design/logos/github-copilot/',
+    author: 'Gil Barbara',
+    authorLink: 'https://github.com/gilbarbara/logos',
+    license: 'CC0 License',
+    licenseLink: 'https://raw.githubusercontent.com/gilbarbara/logos/master/LICENSE.txt',
+    changes: 'none',
+    fieldName: 'Copilot'
+  },
+  {
+    name: 'Arrow Down Alt SVG',
+    nameLink: 'https://www.svgrepo.com/svg/507480/arrow-down-alt',
+    author: 'scarlab',
+    authorLink: 'https://www.svgrepo.com/author/scarlab/',
+    license: 'MIT License',
+    licenseLink: 'https://www.svgrepo.com/page/licensing#MIT',
+    changes: 'none',
+    fieldName: 'arrow down'
+  },
+
+];
+
+export default credits;
