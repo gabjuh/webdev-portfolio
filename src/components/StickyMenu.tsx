b988ebc101ad82eacc2a00e6bc4a5528dbaba584
gabjuh <jjjjgabesz@gmail.com>
2023-04-02 15:38:23 +0200

    create StickyMenu component (experimental)

---
+++ src/components/StickyMenu.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StickyMenu = () => {
+  return (
+    <div className="sticky w-[100%] top-[50vh] right-0">
+      <div className="relative z-50">
+        <div className="border text-right">StickyMenu</div>
+        <div className="border text-right">StickyMenu</div>
+        <div className="border text-right">StickyMenu</div>
+        <div className="border text-right">StickyMenu</div>
+      </div>
+    </div>
+  );
+};
+
+export default StickyMenu;
