a521e4b7ee071d251a9edff4d89bd2c539215ad4
gabjuh <jjjjgabesz@gmail.com>
2023-04-02 15:39:57 +0200

    import StickyMenu

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,27 @@ import Stack from './components/Stack';
 import Footer from './components/Footer';
 import Chat from './components/Chat';
 import Credits from './components/Credits/Credits';
+import StickyMenu from './components/StickyMenu';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={
-          <div className="z-0">
-            <Stack />
-            <CvTree />
-            <Chat />
-          </div>
-        } />
-        <Route path="/credits" element={<Credits />} />
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      <div className="App">
+        {/* <StickyMenu /> */}
+
+        <Routes>
+          <Route path="/" element={
+            <div className="z-0">
+              <Stack />
+              <CvTree />
+              <Chat />
+            </div>
+          } />
+          <Route path="/credits" element={<Credits />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
