b6095f5f0728e2cceaf0fe080c3bd74f38463325
gabjuh <jjjjgabesz@gmail.com>
2023-04-04 00:49:59 +0200

    add images

--- src/data/tree/images.ts
+++ src/data/tree/images.ts
@@ -1,5 +1,8 @@
 import boy from '../../assets/baby-sculptur-boy.png';
 import girl from '../../assets/baby-sculptur-girl.png';
+import wedding from '../../assets/wedding5.png';
+import drive from '../../assets/drive.png';
+import baby from '../../assets/baby3.png';
 
 interface IImages {
   name: string;
@@ -14,6 +17,18 @@ const images: IImages[] = [
   {
     "name": "girl",
     "img": girl
+  },
+  {
+    "name": "wedding",
+    "img": wedding
+  },
+  {
+    "name": "drive",
+    "img": drive
+  },
+  {
+    "name": "baby",
+    "img": baby
   }
 ];
 
