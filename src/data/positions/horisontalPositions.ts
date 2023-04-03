08c51fcf3e3cc7961c8caecef158cc9fc5f1abe7
gabjuh <jjjjgabesz@gmail.com>
2023-04-03 17:09:39 +0200

    move horisiontalPositions into a separate filke (WIP)

---
+++ src/data/positions/horisontalPositions.ts
@@ -0,0 +1,30 @@
+export const horisontalPositions = {
+  sm: {
+    breakpoint: 470,
+    timeline: 300,
+    titlePos: 0,
+    popupPos: - 175,
+    textPos: 0
+  },
+  md: {
+    breakpoint: 860,
+    timeline: 300,
+    titlePos: - 51,
+    popupPos: 115,
+    textPos: - 51
+  },
+  lg: {
+    breakpoint: 1100,
+    timeline: 300,
+    titlePos: - 45,
+    popupPos: 450,
+    textPos: - 45
+  },
+  xl: {
+    breakpoint: 1400,
+    timeline: 300,
+    titlePos: 30,
+    popupPos: 570,
+    textPos: 30
+  }
+};
