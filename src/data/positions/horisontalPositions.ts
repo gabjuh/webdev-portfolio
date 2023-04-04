453d33f8bb16879f36791822211449d88334f733
gabjuh <jjjjgabesz@gmail.com>
2023-04-04 15:07:57 +0200

    add yearPos, xs and 2xl, finetune positioning

--- src/data/positions/horisontalPositions.ts
+++ src/data/positions/horisontalPositions.ts
@@ -1,42 +1,126 @@
 
-// Horizontal position of the title and popup
 export const horisontalPositions = {
-  sm: {
+  'xs': {
     breakpoint: 470,
-    timelinePos: 200,
+    timelinePos: 240,
+    yearPos: -180,
     titlePos: 150,
-    popupPos: - 175,
+    popupPos: - 202,
     textPos: 0
   },
-  md: {
-    breakpoint: 860,
-    timelinePos: 180,
+  'sm': {
+    breakpoint: 640,
+    timelinePos: 200,
+    yearPos: -160,
+    titlePos: -70,
+    popupPos: - 80,
+    textPos: -70
+  },
+  'md': {
+    breakpoint: 768,
+    timelinePos: 208,
+    yearPos: -180,
     titlePos: - 51,
     popupPos: 115,
     textPos: - 51
   },
-  lg: {
-    breakpoint: 1100,
-    timelinePos: 180,
-    titlePos: - 45,
-    popupPos: 450,
-    textPos: - 45
+  'lg': {
+    breakpoint: 1024,
+    timelinePos: 175,
+    yearPos: -160,
+    titlePos: - 60,
+    popupPos: 430,
+    textPos: - 60
   },
-  xl: {
-    breakpoint: 1400,
-    timelinePos: 300,
+  'xl': {
+    breakpoint: 1280,
+    timelinePos: 200,
+    yearPos: -180,
+    titlePos: 30,
+    popupPos: 570,
+    textPos: 30
+  },
+  '2xl': {
+    breakpoint: 1536,
+    timelinePos: 200,
+    yearPos: -180,
     titlePos: 30,
     popupPos: 570,
     textPos: 30
   }
 };
 
+// Horizontal position of the title and popup
+// export const horisontalPositions = {
+//   sm: {
+//     breakpoint: 0,
+//     timelinePos: 0,
+//     titlePos: 0,
+//     popupPos: 0,
+//     textPos: 0
+//   },
+//   md: {
+//     breakpoint: 0,
+//     timelinePos: 0,
+//     titlePos: 0,
+//     popupPos: 0,
+//     textPos: 0
+//   },
+//   lg: {
+//     breakpoint: 0,
+//     timelinePos: 0,
+//     titlePos: 0,
+//     popupPos: 0,
+//     textPos: 0
+//   },
+//   xl: {
+//     breakpoint: 0,
+//     timelinePos: 0,
+//     titlePos: 0,
+//     popupPos: 0,
+//     textPos: 0
+//   }
+// };
+
+// export const horisontalPositions = {
+//   sm: {
+//     breakpoint: 470,
+//     timelinePos: 200,
+//     titlePos: 150,
+//     popupPos: - 175,
+//     textPos: 0
+//   },
+//   md: {
+//     breakpoint: 860,
+//     timelinePos: 180,
+//     titlePos: - 51,
+//     popupPos: 115,
+//     textPos: - 51
+//   },
+//   lg: {
+//     breakpoint: 1100,
+//     timelinePos: 180,
+//     titlePos: - 45,
+//     popupPos: 450,
+//     textPos: - 45
+//   },
+//   xl: {
+//     breakpoint: 1400,
+//     timelinePos: 300,
+//     titlePos: 30,
+//     popupPos: 570,
+//     textPos: 30
+//   }
+// };
+
 // Breakpoints for the view width
 export const breakpoints = {
-  sm: horisontalPositions.sm.breakpoint,
-  md: horisontalPositions.md.breakpoint,
-  lg: horisontalPositions.lg.breakpoint,
-  xl: horisontalPositions.xl.breakpoint
+  'xs': horisontalPositions.xs.breakpoint,
+  'sm': horisontalPositions.sm.breakpoint,
+  'md': horisontalPositions.md.breakpoint,
+  'lg': horisontalPositions.lg.breakpoint,
+  'xl': horisontalPositions.xl.breakpoint,
+  '2xl': horisontalPositions['2xl'].breakpoint
 };
 
 
