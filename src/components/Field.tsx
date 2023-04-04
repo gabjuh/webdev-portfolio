eb1d627285ea2f7482306d6809563d50589da58a
gabjuh <jjjjgabesz@gmail.com>
2023-04-04 11:06:17 +0200

    change && to ternary operator

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -133,7 +133,7 @@ const Field: React.FC<IField> = ({
 
   return (
     <div
-      className={`${isBig && 'col-span-2 row-span-2 '} ${!isLicensed && img && 'border-red-500 border-[3px]'} relative group mx-auto rounded-2xl hover:-translate-y-[4px] hover:-translate-x-[1px] transition-all group`}
+      className={`${isBig ? 'col-span-2 row-span-2 ' : ''}${!isLicensed && img ? 'border-red-500 border-[3px] ' : ''}relative group mx-auto rounded-2xl hover:-translate-y-[4px] hover:-translate-x-[1px] transition-all group`}
       style={fieldStyle}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
@@ -145,7 +145,7 @@ const Field: React.FC<IField> = ({
         <div className="absolute top-[50%] -translate-y-[56%] left-0 right-0">
           <span className="absolute -top-3 left-1 text-[#999] text-[.6rem]">{showIndexes && i}</span>
           <img
-            className={`absolute max-w-[50%] group-hover:!opacity-1)] transition-all duration-200 left-0 right-0 mx-auto top-[50%] -translate-y-[50%] group-hover:drop-shadow-[0_0_10px_rgba(50,255,235,.4)] ${name === 'arrow down' && '!w-[15px]'}`}
+            className={`absolute max-w-[50%] group-hover:!opacity-1)] transition-all duration-200 left-0 right-0 mx-auto top-[50%] -translate-y-[50%] group-hover:drop-shadow-[0_0_10px_rgba(50,255,235,.4)] ${name === 'arrow down' ? '!w-[15px]' : ''}`}
             style={{
               opacity: isBig ? .85 : increaseOpacity(0.6, i)
             }}
