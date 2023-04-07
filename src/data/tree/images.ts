e194d6a321f6fdd553829d844b841bba8871778d
gabjuh <jjjjgabesz@gmail.com>
2023-04-07 03:34:06 +0200

    add credits and a new img

--- src/data/tree/images.ts
+++ src/data/tree/images.ts
@@ -3,32 +3,44 @@ import girl from '../../assets/baby-sculptur-girl.png';
 import wedding from '../../assets/wedding5.png';
 import drive from '../../assets/auto_driving.png';
 import baby from '../../assets/baby3.png';
+import transilvano from '../../assets/prisma_il_transilvano.jpg'
 
 interface IImages {
   name: string;
   img: string;
+  credits: string;
 }
 
 const images: IImages[] = [
   {
     "name": "boy",
-    "img": boy
+    "img": boy,
+    "credits": "AI Art Generator - Hotpot©"
   },
   {
     "name": "girl",
-    "img": girl
+    "img": girl,
+    "credits": "AI Art Generator - Hotpot©"
   },
   {
     "name": "wedding",
-    "img": wedding
+    "img": wedding,
+    "credits": "AI Art Generator - Hotpot©"
   },
   {
     "name": "drive",
-    "img": drive
+    "img": drive,
+    "credits": "AI Art Generator - Gábor Juhász©"
+  },
+  {
+    "name": "prisma",
+    "img": transilvano,
+    "credits": "Sendesaal Bremen©"
   },
   {
     "name": "baby",
-    "img": baby
+    "img": baby,
+    "credits": "AI Art Generator - Hotpot©"
   }
 ];
 
