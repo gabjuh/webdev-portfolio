09c29a60cd8f0289d6d650d778b29a50e4e2a759
gabjuh <jjjjgabesz@gmail.com>
2023-04-07 14:33:11 +0200

    add images

--- src/data/tree/images.ts
+++ src/data/tree/images.ts
@@ -4,6 +4,12 @@ import wedding from '../../assets/wedding5.png';
 import drive from '../../assets/auto_driving.png';
 import baby from '../../assets/baby3.png';
 import transilvano from '../../assets/prisma_il_transilvano.jpg'
+import inline from '../../assets/inline_preview_2.png';
+import musikerPortfolio from '../../assets/musiker_portfolio_2.png';
+import inDialogo from '../../assets/in_dialogo.png';
+import memory from '../../assets/memory_2.jpg';
+import puzzle from '../../assets/puzzle.png';
+import stablegram from '../../assets/stablegram.png';
 
 interface IImages {
   name: string;
@@ -41,7 +47,37 @@ const images: IImages[] = [
     "name": "baby",
     "img": baby,
     "credits": "AI Art Generator - Hotpot©"
-  }
+  },
+  {
+    "name": "inline_preview",
+    "img": inline,
+    "credits": ""
+  },
+  {
+    "name": "musiker_portfolio",
+    "img": musikerPortfolio,
+    "credits": "Concierto Iberico©"
+  },
+  {
+    "name": "in_dialogo",
+    "img": inDialogo,
+    "credits": "Concerto Foscari©"
+  },
+  {
+    "name": "memory_2",
+    "img": memory,
+    "credits": "Projekt für darstellen Memories"
+  },
+  {
+    "name": "puzzle",
+    "img": puzzle,
+    "credits": "Schiebepuzzle (React)"
+  },
+  {
+    "name": "stablegram",
+    "img": stablegram,
+    "credits": "Stablegram (Basic Instagram clone with Laravel)"
+  },
 ];
 
 export default images;
