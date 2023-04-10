22eeef90c5047c5bd54ffc813f145d0201f0aebc
gabjuh <jjjjgabesz@gmail.com>
2023-04-10 13:53:40 +0200

    export fieldstyles interface

---
+++ src/interfaces/FieldStyles.ts
@@ -0,0 +1,10 @@
+export interface IFieldStyles {
+  width: string,
+  height: string,
+  left: string,
+  right: number,
+  background: string;
+  boxShadow: string;
+  transform?: string;
+  transitionDuration: string;
+}
