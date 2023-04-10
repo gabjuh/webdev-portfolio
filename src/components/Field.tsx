12f001fc634addd6ca125681cc579ba78c572f20
gabjuh <jjjjgabesz@gmail.com>
2023-04-10 13:52:54 +0200

    move styles and helpers to generateFieldStyle

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+
+import { IFieldStyles } from '../interfaces/FieldStyles';
+import { useFieldStyles } from '../helpers/generateFieldStyle';
 
 interface IField {
   name: string;
   img: string;
   size: number[];
-  isBig: Boolean;
+  isBig: boolean;
   pos: number[];
   i: number;
-  showIndexes: Boolean;
+  showIndexes: boolean;
   cols: number;
   activeField: number | null;
   setActiveField: (i: number | null) => void;
@@ -35,27 +38,13 @@ const Field: React.FC<IField> = ({
   isLicensed
 }) => {
 
-  const increaseOpacity = (startOp: number, i: number) => startOp + i * 0.001;
-
-  const setGradientBase = (colorValue: number, index: number): number => (255 - colorValue) / nrOfFields * index + colorValue;
-
-  const generateFirstColor = (i: number, cols: number): string => {
-    const [r, g, b, a] = [setGradientBase(25, i), setGradientBase(175, i), setGradientBase(117, i), increaseOpacity(0.3, i)];
-    return `rgba(${r},${g},${b},${a})`;
-  };
-
-  const generateSecondColor = (i: number, cols: number): string => {
-    const [r, g, b, a] = [setGradientBase(120, i), setGradientBase(192, i), setGradientBase(192, i), increaseOpacity(0.3, i)];
-    return `rgba(${r},${g},${b},${a})`;
-  };
-
-  // Get the last digit of the number or return the number if it is smaller than 10
-  const getLastDigit = (nr: number): number => nr % cols;
-
-  // Get the first digit of the number or return 0 if it is smaller than 10
-  const getFirstDigit = (nr: number): number => Math.floor(nr / cols);
-
-  const increaseNrWithIndex = (nr: number, index: number, fineTuning: number = 1): number => (nr + index) * fineTuning;
+  const { stylesDefault, stylesOnHover, stylesOnClick, increaseOpacity } = useFieldStyles(
+    nrOfFields,
+    cols,
+    i,
+    size,
+    pos,
+  )
 
   const handleMouseEnter = () => {
     setFieldStyle(stylesOnHover)
@@ -81,49 +70,6 @@ const Field: React.FC<IField> = ({
     setFieldStyle(stylesOnClick);
     // setActiveField(null);
   }
-
-  interface IFieldStyles {
-    width: string,
-    height: string,
-    left: string,
-    right: number,
-    background: string;
-    boxShadow: string;
-    transform?: string;
-    transitionDuration: string;
-  }
-
-  const stylesDefault: IFieldStyles = {
-    width: `${size[0]}px`,
-    height: `${size[1]}px`,
-    left: `${pos[0]}px`,
-    right: 0,
-    background: `linear-gradient(120deg, ${generateFirstColor(i, cols)}, ${generateSecondColor(i, cols)})`,
-    boxShadow: `${increaseNrWithIndex(-5, getLastDigit(i), .4)}px ${increaseNrWithIndex(-5, getFirstDigit(i), .3)}px 4px rgba(56,97,109,${increaseOpacity(0.45, i)})`,
-    transitionDuration: "600ms",
-  };
-
-  const stylesOnHover: IFieldStyles = {
-    width: `${size[0]}px`,
-    height: `${size[1]}px`,
-    left: `${pos[0]}px`,
-    right: 0,
-    background: `linear-gradient(120deg, ${generateFirstColor(i, cols)}, ${generateSecondColor(i, cols)})`,
-    boxShadow: `${increaseNrWithIndex(-5, getLastDigit(i), 1.1)}px ${increaseNrWithIndex(-5, getFirstDigit(i), 1.1)}px 6px rgba(56,97,109,${increaseOpacity(0.45, i)})`,
-    transform: `translateY(${-4}px) translateX(${-1}px)`,
-    transitionDuration: "150ms",
-  };
-
-  const stylesOnClick: IFieldStyles = {
-    width: `${size[0]}px`,
-    height: `${size[1]}px`,
-    left: `${pos[0]}px`,
-    right: 0,
-    background: `linear-gradient(120deg, ${generateFirstColor(i, cols)}, ${generateSecondColor(i, cols)})`,
-    boxShadow: `${increaseNrWithIndex(-5, getLastDigit(i), .4)}px ${increaseNrWithIndex(-5, getFirstDigit(i), .4)}px 4px rgba(56,97,109,${increaseOpacity(0.45, i)})`,
-    transform: `translateY(${2}px) translateX(${1}px)`,
-    transitionDuration: "100ms",
-  };
 
   const [fieldStyle, setFieldStyle] = useState<IFieldStyles>(stylesDefault);
 
