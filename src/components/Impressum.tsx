18996d2e289c73a9af28981ff0db69095f0cca14
gabjuh <jjjjgabesz@gmail.com>
2023-04-17 14:55:50 +0200

    create impressum and add content

---
+++ src/components/Impressum.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Impressum = () => {
+  return (
+    <div className="container mx-auto p-4 mb-10">
+      <h2 className="my-5 text-3xl">Impressum</h2>
+
+      <p className="my-3 text-xl">Angaben gemäß § 5 TMG</p>
+
+      <p className="text-lg">Gábor Juhász</p>
+      <p className="text-lg">Heinrich-Goebel-Str 43</p>
+      <p className="text-lg">28357 Bremen</p>
+
+      <p className="mt-3 font-bold text-xl">Kontakt:</p>
+      <p className="text-lg">Telefon: +49/151 1099 89 81</p>
+      <p className="text-lg">E-Mail:
+        <a href="mailto:info@gaborjuhasz.de"> info@gaborjuhasz.de</a>
+      </p>
+
+      <p className="mt-5 mb-3 font-bold text-xl">Haftung für Inhalte</p>
+      <p className="text-lg">Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+      <p className="mt-5 mb-3 font-bold text-xl">Haftung für Links</p>
+      <p className="text-lg">Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+      <p className="mt-5 mb-3 font-bold text-xl">Urheberrecht</p>
+      <p className="text-lg">Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+      <p className="mt-5 mb-3 font-bold text-xl">Datenschutz</p>
+      <p className="text-lg">Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.</p>
+
+      <p className="mt-5 text-md">Impressum vom Impressum Generator der <a href="https://www.kanzlei-hasselbach.de/" target="_blank">Kanzlei Hasselbach, Rechtsanwälte für Arbeitsrecht und Familienrecht</a></p>
+
+    </div>
+  );
+};
+
+export default Impressum;
