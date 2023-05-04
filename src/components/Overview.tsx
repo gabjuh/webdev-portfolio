a36bb29bbd66c65fa54ae50aef70209d2a2e82bb
gabjuh <jjjjgabesz@gmail.com>
2023-05-04 17:38:41 +0200

    import images, set design for all of the content

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import raw_data from '../data/cv/cv_gj2.json';
 import { ITree, IGeneral, IItem, IPoint, IContent, ITimeline } from '../interfaces/Tree';
 import ICategories from '../interfaces/Categories';
+import images from '../data/tree/images';
+
 
 const Overview = () => {
 
@@ -24,24 +26,30 @@ const Overview = () => {
         {itProjectItems.map((item, index) => {
           return (
             <div key={index} className="flex flex-row">
-              <div className="flex-row text-right">
-                <div className="">
-                  <p>{item.content.year}</p>
+              {/* Texts */}
+              <div className="relative flex-row text-right w-3/5">
+                <div className="absolute top-[50%] right-[3rem] -translate-y-[50%]">
+                  <div className="mb-5 text-[#eee] font-bold bg-gradient-to-r from-[#eee] via-[#eee] to-[#2ea18c] to-90%">
+                    <p className="mr-2">{item.content.year}</p>
+                  </div>
+                  <div className="text-2xl font-semibold italic">
+                    <p>{item.content.name}</p>
+                  </div>
+                  <div className="text-2xl font-semibold italic">
+                    <p>{item.content.institute}</p>
+                  </div>
+                  <div className="my-4 text-[#0009] font-extralight">
+                    <p>{item.content.description}</p>
+                  </div>
+                  <div className="">
+                    <span className="">{item.content.tech?.map((tech: string) => <div className="inline-block ml-2 text-[#eee] bg-[#555] px-2 rounded-xl text-sm">{tech}</div>)}</span>
+                  </div>
                 </div>
-                <div className="">
-                  <p>{item.content.name}</p>
-                </div>
-                <div className="">
-                  <p>{item.content.institute}</p>
-                </div>
-                <div className="">
-                  <p>{item.content.description}</p>
-                </div>
-                <div className="">
-                  <span className="">{item.content.tech?.map((tech: string) => <div className="inline-block ml-2">{tech}</div>)}</span>
-                </div>
-                <img src="" alt="" />
+              </div>
 
+              {/* Image */}
+              <div className="max-w-[500px]">
+                <img src={images.filter(img => img.name === item.content.image)[0].img} alt={item.content.slug} />
               </div>
             </div>
           );
