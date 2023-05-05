d460d89d3e225f6baef5157f255f0e0fd68cadea
gabjuh <jjjjgabesz@gmail.com>
2023-05-05 13:06:30 +0200

    create icons collection

---
+++ src/data/overview/icons.ts
@@ -0,0 +1,141 @@
+import reactLogo from '../../assets/logos/react_mc.svg';
+import typescriptLogo from '../../assets/logos/typescript_mc.svg';
+import tailwindLogo from '../../assets/logos/tailwind_mc.svg';
+import htmlLogo from '../../assets/logos/html_mc.svg';
+import cssLogo from '../../assets/logos/css_mc.svg';
+import jsLogo from '../../assets/logos/js_mc.svg';
+import bootstrapLogo from '../../assets/logos/bootstrap_mc.svg';
+import nodejsLogo from '../../assets/logos/nodejs_mc.svg';
+import phpLogo from '../../assets/logos/php_mc.svg';
+import mysqlLogo from '../../assets/logos/mysql_mc.svg';
+import wordpressLogo from '../../assets/logos/wordpress_mc.svg';
+import joomlaLogo from '../../assets/logos/joomla_mc.svg';
+import laravelLogo from '../../assets/logos/laravel_mc.svg';
+import sqliteLogo from '../../assets/logos/sqlite_mc.svg';
+import gitLogo from '../../assets/logos/git_mc.svg';
+import sassLogo from '../../assets/logos/sass_mc.svg';
+import psLogo from '../../assets/logos/ps_mc.svg';
+import vsCodeLogo from '../../assets/logos/vscode_mc.svg';
+import indesignLogo from '../../assets/logos/indesign_mc.svg';
+import copilotLogo from '../../assets/logos/copilot_mc.svg';
+import stackOverflowLogo from '../../assets/logos/stack_overflow_mc.svg';
+import githubLogo from '../../assets/logos/github_mc.svg';
+import chatGptLogo from '../../assets/logos/chatgpt.svg';
+import computer from '../../assets/logos/computer_mc.svg';
+import graph from '../../assets/logos/graph_mc.svg';
+import chip from '../../assets/logos/chip_mc.svg';
+
+interface IIcons {
+  name: string;
+  img: string;
+}
+
+const icons: IIcons[] = [
+  {
+    "name": "React",
+    "img": reactLogo
+  },
+  {
+    "name": 'TypeScript',
+    "img": typescriptLogo
+  },
+  {
+    "name": "Tailwind CSS",
+    "img": tailwindLogo
+  },
+  {
+    "name": 'HTML5',
+    "img": htmlLogo
+  },
+  {
+    "name": 'CSS4',
+    "img": cssLogo
+  },
+  {
+    "name": "JavaScript",
+    "img": jsLogo
+  },
+  {
+    "name": 'Bootstrap 4',
+    "img": bootstrapLogo
+  },
+  {
+    "name": 'Node.JS',
+    "img": nodejsLogo
+  },
+  {
+    "name": 'PHP8',
+    "img": phpLogo
+  },
+  {
+    "name": 'MySQL',
+    "img": mysqlLogo
+  },
+  {
+    "name": 'Wordpress',
+    "img": wordpressLogo
+  },
+  {
+    "name": 'Joomla',
+    "img": joomlaLogo
+  },
+  {
+    "name": 'Laravel 8',
+    "img": laravelLogo
+  },
+  {
+    "name": 'SQLite',
+    "img": sqliteLogo
+  },
+  {
+    "name": 'git',
+    "img": gitLogo
+  },
+  {
+    "name": 'Sass',
+    "img": sassLogo
+  },
+  {
+    "name": 'VSCode',
+    "img": vsCodeLogo
+  },
+  {
+    "name": 'ChatGPT',
+    "img": chatGptLogo
+  },
+  {
+    "name": 'Github Copilot',
+    "img": copilotLogo
+  }
+  ,
+  {
+    "name": 'Photoshop CC',
+    "img": psLogo
+  },
+  {
+    "name": 'InDesign CC',
+    "img": indesignLogo
+  },
+  {
+    "name": 'Stack Overflow',
+    "img": stackOverflowLogo
+  },
+  {
+    "name": 'Github',
+    "img": githubLogo
+  },
+  {
+    "name": 'IT',
+    "img": computer
+  },
+  {
+    "name": 'wirtschaft',
+    "img": graph
+  },
+  {
+    "name": 'electrotechnik',
+    "img": chip
+  }
+];
+
+export default icons;
