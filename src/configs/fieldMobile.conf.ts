671a57810767946de752792d869f7bd264320c9c
gabjuh <jjjjgabesz@gmail.com>
2023-05-07 11:43:03 +0200

    create mobile version

---
+++ src/configs/fieldMobile.conf.ts
@@ -0,0 +1,9 @@
+import IFieldConfig from '../interfaces/FieldConfig';
+
+const fieldConfig: IFieldConfig = {
+  size: 45,
+  bigFieldSizeFactor: 2.35,
+  fieldColor: '#464'
+};
+
+export default fieldConfig;
---
+++ src/configs/fieldsMobile.conf.ts
@@ -0,0 +1,9 @@
+import IFieldsConfig from '../interfaces/FieldsConfig';
+
+const fieldsConfigMobile: IFieldsConfig = {
+  cols: 10,
+  gap: 12,
+  nrOfFields: 90
+};
+
+export default fieldsConfigMobile;
