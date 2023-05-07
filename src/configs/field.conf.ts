8b3a4ee59506989fecdee9365161ebb91aabc916
gabjuh <jjjjgabesz@gmail.com>
2023-05-07 12:56:32 +0200

    create field config file

---
+++ src/configs/field.conf.ts
@@ -0,0 +1,9 @@
+import IFieldConfig from '../interfaces/FieldConfig';
+
+const fieldConfig: IFieldConfig = {
+  size: 60,
+  bigFieldSizeFactor: 2.3,
+  fieldColor: '#464'
+};
+
+export default fieldConfig;
