9aeb4a1ffb6ff27fb0e5a8ebda8cb4ecb37ebd09
gabjuh <jjjjgabesz@gmail.com>
2023-05-07 17:42:17 +0200

    activate business card

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
               <Stack />
               <Overview />
               <CvTree />
-              {/* <BusinessCard /> */}
+              <BusinessCard />
               <Chat />
             </div>
           } />
