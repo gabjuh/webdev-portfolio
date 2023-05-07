56c3a346b3640499f953ab914405d8ccca818fff
gabjuh <jjjjgabesz@gmail.com>
2023-05-07 17:34:28 +0200

    create bigfields file and set indexes on mobile

---
+++ src/data/stack/bigFieldsMobile.ts
@@ -0,0 +1,24 @@
+import reactLogo from '../../assets/logos/react_mc.svg';
+import typescriptLogo from '../../assets/logos/typescript_mc.svg';
+import tailwindLogo from '../../assets/logos/tailwind_mc.svg';
+import IBigField from '../../interfaces/BigField';
+
+const bigFields: IBigField[] = [
+  {
+    "index": 21,
+    "name": "React",
+    "img": reactLogo
+  },
+  {
+    "index": 25,
+    "name": 'TypeScript',
+    "img": typescriptLogo
+  },
+  {
+    "index": 33,
+    "name": "Tailwind CSS",
+    "img": tailwindLogo
+  }
+];
+
+export default bigFields;
