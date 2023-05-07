99cb93689742c846b27f2f7950caea7ef86c947a
gabjuh <jjjjgabesz@gmail.com>
2023-05-07 17:37:32 +0200

    remove files

--- src/config/field.conf.ts
+++
@@ -1,9 +0,0 @@
-import IFieldConfig from '../interfaces/FieldConfig';
-
-const fieldConfig: IFieldConfig = {
-  size: 60,
-  bigFieldSizeFactor: 2.3,
-  fieldColor: '#464'
-}
-
-export default fieldConfig;
--- src/config/fields.conf.ts
+++
@@ -1,9 +0,0 @@
-import IFieldsConfig from '../interfaces/FieldsConfig';
-
-const fieldsConfig: IFieldsConfig = {
-  cols: 10,
-  gap: 17,
-  nrOfFields: 90
-};
-
-export default fieldsConfig;
