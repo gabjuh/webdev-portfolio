473420ed0a7e2201e93cfc3f501cb1997d113b94
gabjuh <jjjjgabesz@gmail.com>
2023-09-25 12:02:48 +0200

    change anker behaviour, set last updated data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ import linkedIn from '../assets/logos/linkedin_mc.svg';
 import facebook from '../assets/logos/facebook_mc.svg';
 import github from '../assets/logos/github_mc.svg';
 import stackOverflow from '../assets/logos/stack_overflow_mc.svg';
-import cv from '../assets/pdfs/Lebenslauf_Gabor_Juhasz_19.04.2023.pdf'
+import cv from '../assets/pdfs/GaborJuhasz_CV_22.09.2023.pdf'
 
 const Footer = () => {
   return (
     <footer className="p-10 bg-[#ddd] w-[100%]">
       <p className="text-center">
-        <a href={cv} className="text-[#7a7] hover:text-[#777] transition-all duration-200">CV</a> | <a href="mailto:info@gaborjuhasz.de" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Kontakt</a> | <Link to="/credits" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Credits</Link> | <Link to="/impressum" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Impressum</Link>
+        <a href={cv} className="text-[#7a7] hover:text-[#777] transition-all duration-200" target="_blank">CV</a> | <a href="mailto:info@gaborjuhasz.de" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Kontakt</a> | <Link to="/credits" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Credits</Link> | <Link to="/impressum" className="text-[#7a7] hover:text-[#777] transition-all duration-200">Impressum</Link>
       </p>
       <div className="flex justify-center">
         <a className="w-[45px] p-3" href="https://www.linkedin.com/in/g%C3%A1bor-juh%C3%A1sz-5352935a/" target="_blank">
@@ -31,8 +31,8 @@ const Footer = () => {
           <img src={facebook} alt="Facebook Icon" />
         </a>
       </div>
-      <p className="text-center w-[100%] text-sm">Ⓒ Gábor Juhász 2023</p>
-      <p className="text-center w-[100%] text-sm text-[#999]">Mai 10. Version</p>
+      <p className="text-center w-[100%] text-sm">Ⓒ Gábor Juhász</p>
+      <p className="text-center w-[100%] text-sm text-[#999]">Stand: 25. September 2023</p>
     </footer>
   );
 };
