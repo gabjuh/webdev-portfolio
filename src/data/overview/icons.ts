395e5499de7c7259f513db2d49fe0ac1c38938f6
gaborjuhasz <adgbe@yahoo.de>
2025-04-03 12:45:38 +0200

    add new icons

--- src/data/overview/icons.ts
+++ src/data/overview/icons.ts
@@ -1,34 +1,40 @@
-import reactLogo from '../../assets/logos/react_mc.svg';
-import typescriptLogo from '../../assets/logos/typescript_mc.svg';
-import tailwindLogo from '../../assets/logos/tailwind_mc.svg';
-import htmlLogo from '../../assets/logos/html_mc.svg';
-import cssLogo from '../../assets/logos/css_mc.svg';
-import jsLogo from '../../assets/logos/js_mc.svg';
+import accessibility from '../../assets/logos/accessibility-mc.svg';
+import angular from '../../assets/logos/angular_mc.svg';
 import bootstrapLogo from '../../assets/logos/bootstrap_mc.svg';
+import cSharp from '../../assets/logos/c-sharp_mc.svg';
+import chatGptLogo from '../../assets/logos/chatgpt.svg';
+import chip from '../../assets/logos/chip_mc.svg';
+import computer from '../../assets/logos/computer_mc.svg';
+import copilotLogo from '../../assets/logos/copilot_mc.svg';
+import cssLogo from '../../assets/logos/css_mc.svg';
+import dotNet from '../../assets/logos/dotnet_mc.png';
+import gitLogo from '../../assets/logos/git_mc.svg';
+import githubLogo from '../../assets/logos/github_mc.svg';
+import graph from '../../assets/logos/graph_mc.svg';
+import htmlLogo from '../../assets/logos/html_mc.svg';
+import indesignLogo from '../../assets/logos/indesign_mc.svg';
+import jira from '../../assets/logos/jira_mc.svg';
+import job from '../../assets/logos/job_mc.svg';
+import joomlaLogo from '../../assets/logos/joomla_mc.svg';
+import jsLogo from '../../assets/logos/js_mc.svg';
+import laravelLogo from '../../assets/logos/laravel_mc.svg';
+import menuArrow from '../../assets/logos/menu_arrow_mc.svg';
+import mysqlLogo from '../../assets/logos/mysql_mc.svg';
+import newTab from '../../assets/logos/new-tab_mc.svg';
 import nodejsLogo from '../../assets/logos/nodejs_mc.svg';
 import phpLogo from '../../assets/logos/php_mc.svg';
-import mysqlLogo from '../../assets/logos/mysql_mc.svg';
-import wordpressLogo from '../../assets/logos/wordpress_mc.svg';
-import joomlaLogo from '../../assets/logos/joomla_mc.svg';
-import laravelLogo from '../../assets/logos/laravel_mc.svg';
-import sqliteLogo from '../../assets/logos/sqlite_mc.svg';
-import gitLogo from '../../assets/logos/git_mc.svg';
-import sassLogo from '../../assets/logos/sass_mc.svg';
 import psLogo from '../../assets/logos/ps_mc.svg';
-import vsCodeLogo from '../../assets/logos/vscode_mc.svg';
-import indesignLogo from '../../assets/logos/indesign_mc.svg';
-import copilotLogo from '../../assets/logos/copilot_mc.svg';
-import stackOverflowLogo from '../../assets/logos/stack_overflow_mc.svg';
-import githubLogo from '../../assets/logos/github_mc.svg';
-import chatGptLogo from '../../assets/logos/chatgpt.svg';
-import computer from '../../assets/logos/computer_mc.svg';
-import graph from '../../assets/logos/graph_mc.svg';
-import chip from '../../assets/logos/chip_mc.svg';
-import menuArrow from '../../assets/logos/menu_arrow_mc.svg';
-import cSharp from '../../assets/logos/c-sharp_mc.svg';
-import dotNet from '../../assets/logos/dotnet_mc.png';
-import unitTesting from '../../assets/logos/unit_mc.png';
+import reactLogo from '../../assets/logos/react_mc.svg';
+import sassLogo from '../../assets/logos/sass_mc.svg';
+import school from '../../assets/logos/school_mc.svg';
 import scrum from '../../assets/logos/scrum_mc.svg';
+import sqliteLogo from '../../assets/logos/sqlite_mc.svg';
+import stackOverflowLogo from '../../assets/logos/stack_overflow_mc.svg';
+import tailwindLogo from '../../assets/logos/tailwind_mc.svg';
+import typescriptLogo from '../../assets/logos/typescript_mc.svg';
+import unitTesting from '../../assets/logos/unit_mc.png';
+import vsCodeLogo from '../../assets/logos/vscode_mc.svg';
+import wordpressLogo from '../../assets/logos/wordpress_mc.svg';
 
 interface IIcons {
   name: string;
@@ -160,6 +166,30 @@ const icons: IIcons[] = [
   {
     "name": 'Unit Testing',
     "img": unitTesting
+  },
+  {
+    "name": 'Angular 19',
+    "img": angular
+  },
+  {
+    "name": 'Accessibility',
+    "img": accessibility
+  },
+  {
+    "name": 'Jira',
+    "img": jira
+  },
+  {
+    "name": 'Job',
+    "img": job
+  },
+  {
+    "name": 'New tab',
+    "img": newTab
+  },
+  {
+    "name": 'School',
+    "img": school
   }
 ];
 
