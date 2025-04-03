c4aeaa09365e511b680e715ebb249ac286190391
gaborjuhasz <adgbe@yahoo.de>
2025-04-03 10:03:28 +0200

    fix import issues resort

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
 import './App.css';
-import CvTree from './components/CvTree';
-import Stack from './components/Stack';
-import Footer from './components/Footer';
-import Chat from './components/Chat';
-import Hero from './components/Hero';
-import Credits from './components/Credits/Credits';
-import Impressum from './components/Impressum';
-import BusinessCard from './components/BusinessCard';
-import ScrollProgressBar from './components/ScrollProgressBar';
-import Overview from './components/Overview';
-import StickyMenu from './components/StickyMenu';
 
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import BusinessCard from './components/BusinessCard';
+import Chat from './components/Chat';
+import Credits from './components/Credits/Credits';
+import CvTree from './components/CvTree';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Impressum from './components/Impressum';
+import Overview from './components/Overview';
+import ScrollProgressBar from './components/ScrollProgressBar';
+import Stack from './components/Stack';
+import StickyMenu from './components/StickyMenu';
 
 function App() {
   return (
